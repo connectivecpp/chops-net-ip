678e5de946e5c20d4535977c1f1d0637a0a223fc
Cliff Green <softwarelibre@codewrangler.net>
2017-11-11 12:01:42 -0800

    Initial creation of repeat.hpp

---
+++ include/repeat.hpp
@@ -0,0 +1,41 @@
+/** @file
+ *
+ * @ingroup utility_module
+ *
+ * @brief Repeat code N times, since the C++ standard range-for does not make it
+ * easy to repeat a loop N times. The lambda or function object can access the iteration
+ * count, if desired.
+ *
+ * This code is copied from Vittorio Romeo's blog at https://vittorioromeo.info/. It is 
+ * the same as his, including the noexcept propagation, except that it has been 
+ * simplified to not be templatized on the count type.
+ * 
+ * 
+ * @authors Vittorio Romeo, Cliff Green
+ * @date 2017
+ * 
+ */
+
+#include <type_traits>
+
+template <typename F>
+constexpr auto repeat(int n, F&& f)
+    noexcept(noexcept(f(n)))
+    -> std::enable_if_t<std::is_invocable_v<int, N>>
+{
+    for(int i = 0; i < n; ++i)
+    {
+        f(i);
+    }
+}
+
+template <typename F>
+constexpr auto repeat(int n, F&& f)
+    noexcept(noexcept(f()))
+    -> std::enable_if_t<!std::is_invocable_v<int, N>>
+{
+    for(int i = 0; i < n; ++i)
+    {
+        f();
+    }
+}
