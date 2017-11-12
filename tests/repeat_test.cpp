e0216cb8f3ea0c6a9887af012bf3d5d6e74bb9d9
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-11-11 16:54:32 -0800

    Additional coding for repeat_test_.cpp

--- tests/repeat_test.cpp
+++ tests/repeat_test.cpp
@@ -3,21 +3,49 @@
 
 #include "repeat.hpp"
 
-void repeater ( int N ) {
+int gSum = 0;
 
-  int sum = 0;
-
-
+void myfunc_a () {
+  gSum += 1;
 }
 
-void myfunc ( int i ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+void myfunc_b ( int i ) {
+  gsum += i;
 }
 
+TEST_CASE( "Repeat function test", "[repeat]" ) {
+  gSum = 0;
+  REQUIRE( gSum == 0 );
 
-TEST_CASE( "Repeat function is called" << N << "times", "[repeat]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+  const int N = 50;
+  const int Sum = N * (N + 1) / 2;
+  INFO ("N = " << N << ", Sum = " << Sum);
+
+  SECTION ( "Testing myfunc without index") {
+    repeat(N, myfunc_a);
+    REQUIRE (gSum == Sum);
+  }
+  SECTION ( "Testing myfunc with index") {
+    repeat(N, myfunc_b);
+    REQUIRE (gSum == Sum);
+  }
+  SECTION ( "Testing lambda func without index") {
+    repeat(N, [] { myfunc_a() } );
+    REQUIRE (gSum == Sum);
+  }
+  SECTION ( "Testing lambda func with index") {
+    repeat(N, [] { myfunc_a(int i) } );
+    REQUIRE (gSum == Sum);
+  }
+  SECTION ( "Testing lambda func without index and local var") {
+    int lSum = 0;
+    repeat(N, [&lSum] { lSum += 1; } );
+    REQUIRE (lSum == Sum);
+  }
+  SECTION ( "Testing lambda func with index and local var") {
+    int lSum = 0;
+    repeat(N, [&lSum] (int i) { lSum += i; } );
+    REQUIRE (lSum == Sum);
+  }
+
 }
