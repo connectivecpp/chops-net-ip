b0d92b16d3ddee86594145411fdc2e1d69dea90a
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-11-22 18:49:59 -0800

    Slowly getting there.

--- test/timer/periodic_timer_test.cpp
+++ test/timer/periodic_timer_test.cpp
@@ -11,7 +11,7 @@
 #include "utility/repeat.hpp"
 
 template <typename Clock, typename Dur, typename TP>
-void timer_test (int iterations, Dur timer_dur, Dur wait_time, TP start_time) {
+void timer_test (int expected, Dur timer_dur, Dur wait_time, TP start_time) {
 
   chops::periodic_timer<Clock> timer;
   std::experimental::net::io_context ioc;
@@ -19,12 +19,18 @@ void timer_test (int iterations, Dur timer_dur, Dur wait_time, TP start_time) {
     wg { std::experimental::net::make_work_guard(ioc) };
 
   std::thread thr([&ioc] () { ioc.run(); } );
+  
+  int count = 0;
+  timer.start([&count] (const std::system_error& err, const Dur& elapsed) {
+      ++count;
+      INFO ("count  " << count << ", sys err = " << err.what());
+    }
+  );
   std::this_thread::sleep_for(wait_time);
   wg.reset();
   thr.join();
-
-
-  
+  INFO ("Thread joined");
+  REQUIRE (count == expected);
 }
 
 TEST_CASE( "Testing periodic timer", "[periodic_timer]" ) {
