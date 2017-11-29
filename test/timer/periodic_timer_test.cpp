e805caea43a5ebd36d8f7c4814b3d182d2686993
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-11-29 11:23:34 -0800

    Implemented full test coverage, although interval test values are currently limited.

--- test/timer/periodic_timer_test.cpp
+++ test/timer/periodic_timer_test.cpp
@@ -12,42 +12,97 @@
 #include "timer/periodic_timer.hpp"
 #include "utility/repeat.hpp"
 
-const int Expected = 5;
+const int Expected = 9;
 int count = 0;
 
-SCENARIO ( "A periodic timer can be instantiated on the steady clock", "[periodic_timer_steady_clock]" ) {
+bool lambda_util (const std::error_code& err) {
+  ++count;
+  INFO ("count = " << count << ", err code = " << err.value() << ", " << err.message());
+  return count < Expected;
+}
 
-  GIVEN ( "A steady clock, a duration, and a wait time") {
+using wk_guard = std::experimental::net::executor_work_guard<std::experimental::net::io_context::executor_type>;
+
+void wait_util (int wait, wk_guard& wg, std::thread& thr) {
+  std::this_thread::sleep_for(std::chrono::seconds(wait));
+  wg.reset();
+  thr.join();
+  INFO ("Thread joined");
+}
+
+
+template <typename Clock>
+void test_util () {
+
+  GIVEN ( "A clock and a duration") {
 
     std::experimental::net::io_context ioc;
-    chops::periodic_timer<std::chrono::steady_clock> timer {ioc};
-    std::experimental::net::executor_work_guard<std::experimental::net::io_context::executor_type>
-      wg { std::experimental::net::make_work_guard(ioc) };
+    chops::periodic_timer<Clock> timer {ioc};
+    wk_guard wg { std::experimental::net::make_work_guard(ioc) };
 
     std::thread thr([&ioc] () { ioc.run(); } );
     count = 0;
 
-    WHEN ( "The duration is one second and wait time is 6 seconds" ) {
+    WHEN ( "The duration is one second" ) {
+      timer.start_duration_timer( [] (const std::error_code& err, const typename Clock::duration& elap) { 
+          return lambda_util(err);
+        } , std::chrono::seconds(1));
 
-      timer.start_duration_timer( [] (const std::error_code& err, 
-                                      const typename std::chrono::steady_clock::duration& elapsed) {
-          ++count;
-          INFO ("count = " << count << ", err code = " << err.value() << ", " << err.message());
-          return count < Expected;
-        }, std::chrono::seconds(1));
-
-
-      std::chrono::seconds wait_time {Expected+1};
-
-      std::this_thread::sleep_for(wait_time);
-      wg.reset();
-      thr.join();
-      INFO ("Thread joined");
+      wait_util (Expected+1, wg, thr);
 
       THEN ( "the timer callback count should match expected") {
         REQUIRE (count == Expected);
       }
     }
+    WHEN ( "The duration is two seconds and the start time is 5 seconds in the future" ) {
+      timer.start_duration_timer( [] (const std::error_code& err, const typename Clock::duration& elap) { 
+          return lambda_util(err);
+        } , std::chrono::seconds(2), Clock::now() + std::chrono::seconds(5));
+
+      wait_util(Expected * 2 + 5 + 1, wg, thr);
+
+      THEN ( "the timer callback count should match expected") {
+        REQUIRE (count == Expected);
+      }
+    }
+    WHEN ( "The duration is one second and the timer pops on timepoints" ) {
+      timer.start_timepoint_timer( [] (const std::error_code& err, const typename Clock::duration& elap) { 
+          return lambda_util(err);
+        } , std::chrono::seconds(1));
+
+      wait_util (Expected+1, wg, thr);
+
+      THEN ( "the timer callback count should match expected") {
+        REQUIRE (count == Expected);
+      }
+    }
+    WHEN ( "The duration is two seconds and the timer pops on timepoints starting 5 seconds in the future" ) {
+      timer.start_timepoint_timer( [] (const std::error_code& err, const typename Clock::duration& elap) { 
+          return lambda_util(err);
+        } , std::chrono::seconds(2), Clock::now() + std::chrono::seconds(5));
+
+      wait_util(Expected * 2 + 5 + 1, wg, thr);
+
+      THEN ( "the timer callback count should match expected") {
+        REQUIRE (count == Expected);
+      }
+    }
+
   } // end given
 }
 
+SCENARIO ( "A periodic timer can be instantiated on the steady clock", "[periodic_timer_steady_clock]" ) {
+
+  test_util<std::chrono::steady_clock>();
+
+}
+SCENARIO ( "A periodic timer can be instantiated on the system clock", "[periodic_timer_system_clock]" ) {
+
+  test_util<std::chrono::system_clock>();
+
+}
+SCENARIO ( "A periodic timer can be instantiated on the high resolution clock", "[periodic_timer_high_resolution_clock]" ) {
+
+  test_util<std::chrono::high_resolution_clock>();
+
+}
