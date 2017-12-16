fa745943379a62514a820f6f7671d19bd021a3cc
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-12-16 12:59:24 -0800

    Adding error category to customize std::error_code.

--- include/net_ip/net_ip_exception.hpp
+++ include/net_ip/net_ip_exception.hpp
@@ -2,38 +2,46 @@
  *
  *  @ingroup net_ip_module
  *
- *  @brief Exception classes thrown by Chops @c net_ip library.
+ *  @brief Exception class and error category within Chops @c net_ip library.
  *
  *  @author Cliff Green
- *  @date 2010
+ *  @date 2017, 2018
  *
  */
 
-#ifndef NET_IP_EXCEPTION_HPP_INCLUDED
-#define NET_IP_EXCEPTION_HPP_INCLUDED
+#ifndef NET_IP_ERROR_HPP_INCLUDED
+#define NET_IP_ERROR_HPP_INCLUDED
 
 #include <stdexcept>
 #include <string>
+#include <system_error>
 
 namespace chops {
 namespace net {
+
+enum class net_ip_errc {
+  message_handler_terminated = 1;
+  weak_ptr_expired = 2;
+};
+
+namespace std {
+  template <>
+    struct is_error_code_enum<net_ip_errc> : true_type {};
+}
+
+inline std::error_code make_error_code(net_ip_errc e) {
+  return { static_cast<int>(e), net_ip_errc };
+}
 
 /**
  *  @brief General @c net_ip exception class.
  */
 class net_ip_exception : public std::runtime_error {
 public:
-  net_ip_exception (const char* what) : 
-    std::runtime_error(std::string("net_ip lib exception: ") + std::string(what)) { }
-};
+  net_ip_exception (const std::error_code& e) : 
+    std::runtime_error("net_ip lib exception") : err(e) { }
 
-/**
- *  @brief Exception class for an invalid network resource.
- */
-class net_ip_resource_exception : public std::runtime_error {
-public:
-  net_ip_resource_exception(const char* what) : 
-    std::runtime_error(std::string("net_ip lib exception, resource invalid for: ") + what) { }
+  std::error_code err;
 };
 
 } // end net namespace
