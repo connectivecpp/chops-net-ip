e09b931520b619fb0e1f442263f396e8ab1516fa
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-12-16 18:10:47 -0800

    Initial commit.

---
+++ test/net_ip/net_ip_error_test.cpp
@@ -0,0 +1,50 @@
+/** @file
+ *
+ *  @ingroup test_module
+ *
+ *  @brief Test scenarios for @c net_ip error and exception code.
+ *
+ *  @author Cliff Green
+ *  @date 2017
+ *  @copyright Cliff Green, MIT License
+ *
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <system_error>
+
+#include "net_ip/net_ip_error.hpp"
+
+void will_throw() {
+  chops::net::net_ip_exception ex(std::make_error_code(chops::net::net_ip_errc::message_handler_terminated));
+  throw ex;
+}
+
+SCENARIO ( "Error code and exception test", "[error_code_exception]" ) {
+
+  GIVEN ("A function that throws a net_ip exception") {
+    WHEN ("The function throws") {
+      THEN ("an exception is present") {
+        REQUIRE_THROWS (will_throw());
+      }
+    }
+  } // end given
+
+  GIVEN ("A function that throws a net_ip exception") {
+    WHEN ("The function throws") {
+      THEN ("the exception will contain a custom error message") {
+        try {
+          will_throw();
+        }
+        catch (const chops::net::net_ip_exception& e) {
+          INFO ("Error code message: " << e.err.message());
+          REQUIRE(e.err);
+        }
+      }
+    }
+  } // end given
+
+}
+
