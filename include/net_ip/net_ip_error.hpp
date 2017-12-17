aab7631baa8c90a99be207c4900c4e6bab6f88d5
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-12-16 18:10:17 -0800

    Design complete, more error codes to be added.

--- include/net_ip/net_ip_error.hpp
+++ include/net_ip/net_ip_error.hpp
@@ -4,6 +4,9 @@
  *
  *  @brief Exception class and error category within Chops @c net_ip library.
  *
+ *  The @c error_code customization is inspired by code from Andrzej Krzemienski's and 
+ *  Bjorn Reese's blogs.
+ *
  *  @author Cliff Green
  *  @date 2017, 2018
  *
@@ -13,25 +16,56 @@
 #define NET_IP_ERROR_HPP_INCLUDED
 
 #include <stdexcept>
-#include <string>
 #include <system_error>
+#include <string>
 
 namespace chops {
 namespace net {
 
 enum class net_ip_errc {
-  message_handler_terminated = 1;
-  weak_ptr_expired = 2;
+  message_handler_terminated = 1,
+  weak_ptr_expired = 2,
 };
 
+namespace detail {
+
+struct net_ip_err_category : public std::error_category {
+  virtual const char *name() const noexcept override {
+    return "net_ip_err_category";
+  }
+  virtual std::string message(int val) const override {
+    switch (net_ip_errc(val)) {
+    case net_ip_errc::message_handler_terminated:
+      return "message handler terminated";
+    case net_ip_errc::weak_ptr_expired:
+      return "weak pointer expired";
+    }
+    return "(unknown error)";
+  }
+};
+
+} // end detail namespace
+
+const detail::net_ip_err_category net_ip_err_category_global { };
+
+} // end net namespace
+} // end chops namespace
+
 namespace std {
-  template <>
-    struct is_error_code_enum<net_ip_errc> : true_type {};
+
+template <>
+  struct is_error_code_enum<chops::net::net_ip_errc> : true_type {};
+
+inline error_code make_error_code(chops::net::net_ip_errc e) noexcept {
+  return error_code (static_cast<int>(e), chops::net::net_ip_err_category_global);
 }
 
-inline std::error_code make_error_code(net_ip_errc e) {
-  return { static_cast<int>(e), net_ip_errc };
 }
+
+namespace chops {
+namespace net {
+
+
 
 /**
  *  @brief General @c net_ip exception class.
@@ -39,7 +73,7 @@ inline std::error_code make_error_code(net_ip_errc e) {
 class net_ip_exception : public std::runtime_error {
 public:
   net_ip_exception (const std::error_code& e) : 
-    std::runtime_error("net_ip lib exception") : err(e) { }
+    std::runtime_error("net_ip lib exception"), err(e) { }
 
   std::error_code err;
 };
