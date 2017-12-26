d3a9ab8847dd3dec98df0a01d116869d85e05637
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2017-12-26 10:39:52 -0800

    Initial commit.

---
+++ test/main_test.cpp
@@ -0,0 +1,18 @@
+/** @file
+ *
+ *  @defgroup test_module Unit tests for the various Chops components.
+ *
+ *  @ingroup test_module
+ *
+ *  @brief Source file for combining (i.e. linking) all Chops tests together.
+ *
+ *  @author Cliff Green
+ *  @date 2017, 2018
+ *  @copyright Cliff Green, MIT License
+ *
+ */
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
