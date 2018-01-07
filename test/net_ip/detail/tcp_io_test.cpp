ac0060761343300105baf96b4a8c23c8f50929a1
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-06 23:11:15 -0800

    Crashing, lots of work to do.

--- test/net_ip/detail/tcp_io_test.cpp
+++ test/net_ip/detail/tcp_io_test.cpp
@@ -74,49 +74,6 @@ using thread_promise = std::promise<thread_data>;
 
 using tcp_ioh_ptr = std::shared_ptr<chops::net::detail::tcp_io>;
 
-auto create_start_iohp(ip::tcp::socket sock, entity_notifier& en, bool reply) {
-  using namespace std::placeholders;
-
-  notifier_cb cb(std::bind(&entity_notifier::notify_me, &en, _1, _2));
-
-  auto iohp = std::make_shared<chops::net::detail::tcp_io>(std::move(sock), cb);
-
-  msg_hdlr<chops::net::detail::tcp_io> mh (reply);
-  iohp->start_io(mh, variable_len_msg_frame, 2);
-  
-  return iohp;
-}
-
-struct test_acceptor {
-  ip::tcp::acceptor m_acc;
-  tcp_ioh_ptr       m_ioh_ptr;
-  entity_notifier   m_en;
-  bool              m_reply;
-
-  test_acceptor(io_context& ioc, bool reply) : 
-           m_acc(ioc, ip::tcp::endpoint(ip::address_v4::any(), test_port)), 
-           m_ioh_ptr(), m_en(), m_reply(reply) { }
-
-  void start() {
-    m_acc.async_accept([this] (const std::error_code& err, ip::tcp::socket sock) {
-      auto en_fut = m_en.prom.get_future();
-      auto iohp = create_start_iohp(std::move(sock), m_en, m_reply);
-  }
-
-};
-
-void acceptor_func (thread_promise thr_prom, const vec_buf& in_msg_set, io_context& ioc, 
-                    bool reply) {
-
-  test_acceptor acc(ioc);
-  acc.start();
-
-  auto ret = en_fut.get(); // wait for termination callback
-
-  thr_prom.set_value(thread_data(std::get<0>(ret), std::get<1>(ret) == iohp, in_msg_set == mh.msgs));
-
-}
-
 void connector_func (thread_promise thr_prom, const vec_buf& in_msg_set, io_context& ioc, 
                      int interval) {
   using namespace std::placeholders;
@@ -128,24 +85,53 @@ void connector_func (thread_promise thr_prom, const vec_buf& in_msg_set, io_cont
   ip::tcp::socket sock(ioc);
   sock.connect(ip::tcp::endpoint(ip::make_address(test_addr), test_port));
   auto iohp = std::make_shared<chops::net::detail::tcp_io>(std::move(sock), cb);
-
   msg_hdlr<chops::net::detail::tcp_io> mh (false);
-
   iohp->start_io(mh, variable_len_msg_frame, 2);
 
   for (auto buf : in_msg_set) {
     iohp->send(chops::const_shared_buffer(std::move(buf)));
     std::this_thread::sleep_for(std::chrono::milliseconds(interval));
   }
-  // send empty body, shutdown signal
-  iohp->send(chops::const_shared_buffer(make_variable_len_msg(chops::mutable_shared_buffer())));
   auto ret = en_fut.get(); // wait for termination callback
-
-  thr_prom.set_value(thread_data(std::get<0>(ret), std::get<1>(ret) == iohp, in_msg_set == mh.msgs));
+  thr_prom.set_value(thread_data(std::get<0>(ret), std::get<1>(ret) == iohp, mh.msgs.empty()));
 
 }
 
+void acc_conn_func (const vec_buf& in_msg_set, io_context& ioc, 
+                    bool reply, int interval) {
 
+  using namespace std::placeholders;
+
+  ip::tcp::acceptor acc(ioc, ip::tcp::endpoint(ip::address_v4::any(), test_port));
+
+  entity_notifier en { };
+  auto en_fut = en.prom.get_future();
+  notifier_cb cb(std::bind(&entity_notifier::notify_me, &en, _1, _2));
+
+  INFO ("Creating connector thread, msg interval: " << interval);
+
+  thread_promise conn_prom;
+  auto conn_fut = conn_prom.get_future();
+  std::thread conn_thr(connector_func, std::move(conn_prom), std::cref(in_msg_set), std::ref(ioc), 
+                       interval);
+
+  auto iohp = std::make_shared<chops::net::detail::tcp_io>(std::move(acc.accept()), cb);
+  msg_hdlr<chops::net::detail::tcp_io> mh (reply);
+  iohp->start_io(mh, variable_len_msg_frame, 2);
+
+  auto en_ret = en_fut.get(); // wait for termination callback
+  INFO ("Entity future popped");
+  auto conn_data = conn_fut.get();
+  INFO ("Connector thread future popped, joining thread");
+  conn_thr.join();
+  INFO ("Acceptor error code: " << std::get<0>(en_ret));
+  INFO ("Connector error code: " << std::get<0>(conn_data));
+  REQUIRE (std::get<1>(en_ret) == iohp);
+  REQUIRE (std::get<1>(conn_data));
+  REQUIRE (in_msg_set == mh.msgs);
+  REQUIRE (std::get<2>(conn_data));
+
+}
 
 
 SCENARIO ( "Tcp IO handler test, one-way", "[tcp_io_one_way]" ) {
@@ -159,30 +145,12 @@ SCENARIO ( "Tcp IO handler test, one-way", "[tcp_io_one_way]" ) {
   GIVEN ("An executor work guard and a message set") {
  
     WHEN ("an acceptor and connector are created") {
-      thread_promise acc_prom { };
-      auto acc_fut = acc_prom.get_future();
-      std::thread acc_thr(acceptor_func, std::move(acc_prom), std::cref(ms), std::ref(ioc), 
-                          false);
-      thread_promise conn_prom { };
-      auto conn_fut = conn_prom.get_future();
-      std::thread conn_thr(connector_func, std::move(conn_prom), std::cref(ms), std::ref(ioc), 
-                          Interval);
       THEN ("the futures provide synchronization and data returns") {
-        auto acc_data = acc_fut.get();
-        acc_thr.join();
-        auto conn_data = conn_fut.get();
-        conn_thr.join();
-        INFO ("Acceptor error code: " << std::get<0>(acc_data));
-        INFO ("Connector error code: " << std::get<0>(acc_data));
-        REQUIRE (std::get<1>(acc_data));
-        REQUIRE (std::get<1>(conn_data));
-        REQUIRE (std::get<2>(acc_data));
-        REQUIRE (std::get<2>(conn_data));
+        acc_conn_func (ms, ioc, false, 50);
       }
     }
   } // end given
   wg.reset();
-  run_thr.join();
 }
 
 /*
