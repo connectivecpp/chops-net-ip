0d1236a9d65eb5e3eb2250cfe49e027895bbb42a
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-07 16:53:44 -0800

    Tweaking message flow logic.

--- test/net_ip/detail/shared_utility_test.hpp
+++ test/net_ip/detail/shared_utility_test.hpp
@@ -4,6 +4,19 @@
  *
  *  @brief Declarations for utility code shared between @c net_ip tests.
  *
+ *  The general Chops Net IP test strategy is to have a message sender and message 
+ *  receiver, with a flag specifying whether the receiver is to loop back the
+ *  messages. It is independent of whether the sender or receiver is an acceptor
+ *  or connector. In the test routines, coordination may be needed to know when a 
+ *  connection has been made so that message flow can start. At the higher layers,
+ *  the Chops Net IP library facilities provide the connection notification (state
+ *  change function object callbacks).
+ *
+ *  When the message flow is finished, an empty body message is sent to the receiver. 
+ *  This empty body message is sent back to the sender regardless of the loopback
+ *  flag, and may make an additional trip to the receiver before being discarded 
+ *  (depending on message handler logic).
+ *
  *  @author Cliff Green
  *  @date 2017, 2018
  *  @copyright Cliff Green, MIT License
@@ -43,9 +56,11 @@ vec_buf make_msg_set(F&& func, std::string_view pre, char body_char, int num_msg
       vec.push_back (f(make_body_buf(pre, body_char, i+1)));
     }
   );
-  // add empty body message to signify end of set
-  vec.push_back (func( chops::mutable_shared_buffer{ } ));
   return vec;
+}
+template <typename F>
+chops::mutable_shared_buffer make_empty_body_msg(F&& func) {
+  return func( chops::mutable_shared_buffer{ } );
 }
 
 template <typename IOH>
@@ -53,16 +68,19 @@ struct msg_hdlr {
   using endp_type = typename IOH::endpoint_type;
   using const_buf = std::experimental::net::const_buffer;
 
-  mutable vec_buf msgs;
+  vec_buf& msgs;
   bool reply;
+  mutable bool shutdown_recvd;
 
-  msg_hdlr(bool rep) : msgs(), reply(rep) { }
+  msg_hdlr(vec_buf& m, bool rep) : msgs(m), reply(rep), shutdown_recvd(false) { }
 
   bool operator()(const_buf buf, chops::net::io_interface<IOH> io_intf, endp_type /* endp */) const {
     chops::mutable_shared_buffer sh_buf(buf.data(), buf.size());
-    msgs.push_back(sh_buf);
-    // if last message and not replying, time to shut down
-    return reply ? io_intf.send(std::move(sh_buf)) : (sh_buf.size() > 2);
+    if (sh_buf.size() > 2) { // not a shutdown message
+      msgs.push_back(sh_buf);
+      return reply ? io_intf.send(std::move(sh_buf)) : true;
+    }
+    return shutdown_recvd ? false : (shutdown_recvd = true, io_intf.send(std::move(sh_buf)));
   }
 };
 
