e422e7dbc98e7f93e2ce8b427c106d3317d8f87a
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-10 11:34:20 -0800

    Ready to build and test on net_entity.

--- include/net_ip/net_entity.hpp
+++ include/net_ip/net_entity.hpp
@@ -14,7 +14,10 @@
 #define NET_ENTITY_HPP_INCLUDED
 
 
+#include <memory> // std::shared_ptr, std::weak_ptr
 #include <cstddef> // std::size_t 
+#include <utility> // std::forward
+#include <system_error> // std::make_error
 
 namespace chops {
 namespace net {
@@ -34,9 +37,7 @@ namespace net {
  * 
  *  A @c net_entity object is either associated with a network entity 
  *  (i.e. the @c std::weak pointer is good), or not. The @c is_valid method queries 
- *  if the association is present. Note that even if the @c std::weak_pointer is 
- *  valid, the network entity might be in the process of closing or being 
- *  destructed. 
+ *  if the association is present.
  *
  *  Applications can default construct a @c net_entity object, but it is not useful 
  *  until a valid @c net_entity object is assigned to it (as provided in the make
@@ -46,7 +47,8 @@ namespace net {
  *  to be used in associative or sequence containers.
  *
  *  All @c net_entity methods are safe to call concurrently from multiple
- *  threads.
+ *  threads, although it is questionable logic for multiple threads to call
+ *  @c start or @c stop at the same time.
  *
  */
 
@@ -72,23 +74,28 @@ public:
   net_entity<ET>& operator=(net_entity&&) = default;
 
 /**
- *  @brief Construct an @c net_entity object with a @c SockLib resource.
+ *  @brief Construct with a shared weak pointer to an internal net entity, this is an
+ *  internal constructor only and not to be used by application code.
  *
- *  This constructor is used by @c SockLib internal facilities. It is not meant
- *  to be used by application code.
- *
- *  @note At some point this constructor will be declared @c private, with @c friend
- *  access to appropriate facilities.
- *  
  */
-  net_entity (std::weak_ptr<ET> p) noexcept : m_eh_wptr(p) { }
+
+  explicit net_entity (std::weak_ptr<ET> p) noexcept : m_eh_wptr(p) { }
+
+/**
+ *  @brief Query whether an internal net entity is associated with this object.
+ *
+ *  If @c true, a net entity (TCP acceptor or connector or UDP entity) is associated. 
+ *
+ *  @return @c true if associated with a net entity.
+ */
+  bool is_valid() const noexcept { return !m_eh_wptr.expired(); }
 
 /**
  *  @brief Query whether @c start has been called or not.
  *
  *  @return @c true if @c start has been called, @c false otherwise.
  *
- *  @throw A @c net_ip_exception is thrown if there is not an associated IO handler.
+ *  @throw A @c net_ip_exception is thrown if no association to a net entity.
  */
   bool is_started() const {
     if (auto p = m_eh_wptr.lock()) {
@@ -98,206 +105,163 @@ public:
   }
 
 /**
- *  @brief Query whether this object is valid (associated with a network resource).
+ *  @brief Start network processing on the associated net entity with the application
+ *  providing a state change function object callback.
  *
- *  An @c net_entity object is associated with a @c SockLib network resource
- *  through the @c SockLib @c create methods. If an @c net_entity is default constructed,
- *  it is not in a valid state, and this method will return @c false. If the 
- *  network resource associated with an @c net_entity is destroyed (e.g. the 
- *  creating @c SockLib object is destroyed, for example by calling the @c net_entity @c stop 
- *  method), this method will return @c false.
+ *  Once a net entity (TCP acceptor, TCP connector, UDP entity) is created through 
+ *  a @c net_ip @c make method, @c start is then called, and this is when
+ *  local port binding and other processing (e.g. TCP listen, TCP connect) occurs.
  *
- *  @return @c true if associated network resource is still valid.
+ *  Input and output processing does not start until the @c io_interface @c start_io
+ *  method is called.
+ *
+ *  The application provides a state change function object callback which
+ *  allows the internal net entity to notify the application when one of the 
+ *  following occurs: 1) a TCP connect succeeds, or 2) a TCP accept succeeds, or
+ *  3) a UDP entity is ready.
+ *
+ *  The same state change callback is invoked when processing is stopped due
+ *  to an error or connection close or application close.
+ *
+ *  @c start can be called, followed by @c stop, followed by @c start, etc.
+ *  This may be useful (for example) for a TCP connector that needs to 
+ *  reconnect after a connection has been lost.
+ *
+ *  @param state_chg A function object callback with the following signature:
+ *
+ *  @code
+ *    // TCP io:
+ *    void (chops::net::tcp_io_interface, std::error_code, std::size_t);
+ *    // UDP io:
+ *    void (chops::net::udp_io_interface, std::error_code, std::size_t);
+ *  @endcode
+ *
+ *  The parameters are as follows:
+ *
+ *  1) provides @c io_interface access (@c start_io, @c stop_io, etc) to the 
+ *  underlying IO handler
+ *
+ *  2) error code, which may be zero (no error) or contain a system error or 
+ *  a Chops Net IP error.
+ *
+ *  3) number of underlying IO handlers associated with this net entity; for 
+ *  a TCP connector or a UDP entity the number is either 0 or 1; for a TCP 
+ *  acceptor the number is 0 to N, depending on the number of accepted 
+ *  connections
+ *
+ *  The state change function object callback is always invoked at least once
+ *  after @c start and before any network processing starts, allowing initialization
+ *  of application states (if needed). The values will be:
+ *
+ *  1) a default constructed @c io_interface (@c is_valid() == @c false) 
+ *
+ *  2) zero (non-error) std::error_code 
+ *
+ *  3) 0 (i.e. no connections or binds have occurred)
+ *
+ *  @return @c true if valid net entity association.
+ *
  */
-  bool valid() const { return !mResourcePtr.expired(); }
-
-/**
- *  @brief Query whether the associated network resource is started or not.
- *
- *  The associated network resource is started if the @c start method from an associated
- *  @c net_entity has been called. 
- *
- *  @return @c true if the network resource has been started, @c false otherwise.
- *
- *  @throw @c SockLibException is thrown if the underlying resource is invalid.
- */
-  bool started() const {
-    if (detail::SockLibResourcePtr p = mResourcePtr.lock()) {
-      return p->started();
-    }
-    throw SockLibResourceException("net_entity.started");
+  template <typename F>
+  bool start(F&& state_chg) {
+    auto p = m_eh_wptr.lock();
+    return p ? (p->start(std::forward<F>(state_chg)), true) : false;
   }
 
 /**
- *  @brief Start network processing on the associated network resource.
+ *  @brief Stop network processing on the associated net entity after calling @c stop_io on
+ *  each associated IO handler, invoke the application provided state change function object
+ *  callback.
  *
- *  Once a network resource is created through a @c SockLib @c create method, it
- *  must be started before processing occurs. Requiring a specific @c start method
- *  to be called allows application control of when the port binding and follow-on
- *  processing is to be started (e.g. TCP connect attempt, TCP listen, TCP reads). 
+ *  Stopping the network processing may involve closing connections, deallocating
+ *  resources, and unbinding from ports. 
  *
- *  The @c start method also provides various callback objects to the network
- *  resource. The @c MsgFrame and @c IncomingMsgCb callbacks are provided, as
- *  well as a @c ChannelChangeCb. 
+ *  The state change function object callback will be invoked, possibly multiple times,
+ *  with the following values:
  *
- *  For network resources where reads are not important, a @c start method
- *  with no @c MsgFrame or @c IncomingMsgCb parameters is provided. This might
- *  be a UDP sender, or a TCP acceptor or connector where incoming data is not 
- *  needed.
+ *  1) a valid @c io_interface corresponding to the @c io_interface provided after @c start
  *
- *  A network resource can be stopped, and then later started. The @c stop
- *  method must be called before @c start can be called, even if the network
- *  resource is internally in the equivalent of a "stopped" state. An example
- *  of this would be a TCP Connector where the TCP connection has been broken,
- *  and a connect is to be attempted again.
+ *  2) std::error_code with the value chops::net::net_ip_errc::io_handler_stopped
  *
- *  Errors are reported in one of two ways - immediately, through an exception 
- *  thrown from this method, or later on through the @c ChannelChangeCb callback.
- *  Bind errors, or a TCP listen failure, or other similar errors result in
- *  a thrown exception. Errors that happen later on in the network processing
- *  timeline, such as TCP connect error, or an accept error, or a broken TCP 
- *  connection, result in a @c ChannelChangeCb notification.
+ *  3) 0 / N (count of valid IO handlers or resources for this @c net_entity)
  *
- *  Bind failures are usually due to a port that is already in use.
+ *  Specifically, a TCP connector and a UDP net entity will have one state change callback
+ *  invocation with a IO handler count of 0, while a TCP acceptor will have a state change
+ *  callback invocation for each current connection (and there may be 0 connections), and one 
+ *  additional invocation with a default constructed (i.e. invalid) @c io_interface and a 
+ *  IO handler count of 0.
  *
- *  @param ccCb @c ChannelChangeCb callback, which will provide a @c OutputChannel 
- *  object and other information when a network resource is ready for input and output. 
- *  A function named @c simpleChannelChangeCb is available for applications that
- *  do not care about notification. The @c ChannelChangeCb is copied at least once 
- *  during @c start method invocation, but never copied afterwards (to simplify 
- *  any internal state management). See @c ChannelChangeCb documentation for more 
- *  info. To allow convenient app state initialization, a @c ChannelChangeCb callback 
- *  will be immediately invoked before any network processing is started, with the 
- *  following values:
- *  1) empty @c OutputChannel (default constructed, @c valid() == @c false); 
- *  2) zero (non-error) boost::system::error_code; 
- *  3) 0 (count of valid connections or resources for this @c net_entity).
- *
- *  @param msgFrame Function object or function pointer that provides "message
- *  framing" callback handling. See @c MsgFrame class documentation for more info.
- *  If no application specific message framing is needed, a default @c MsgFrame
- *  object can be used (of type @c SimpleMsgFrame), which reads one byte at a time. 
- *  A @c SimpleMsgFrame object is also appropriate to pass in for UDP sender 
- *  network resources, where reads are not performed. A copy will be made at 
- *  initial @c start method invocation, but once network processing 
- *  has begun no additional copies will be made (simplifying application logic
- *  regarding internal states of the @c MsgFrame or @c IncomingMsgCb objects).
- *
- *  @param incMsgCb Callback of type @c IncomingMsgCb for processing incoming
- *  messages. See @c IncomingMsgCb for more info. If no incoming message processing 
- *  is needed, the @c simpleIncomingMsgCb function can be used, which does
- *  nothing. A copy will be made at @c start method invocation time, but no copies 
- *  will be made once network processing has started.
- *
- *  @return @c false if the resource is already started, @c true otherwise.
- *
- *  @throw @c SockLibException is thrown if the underlying resource is invalid. A
- *  @c boost::system::system_error is thrown if the immediate start processing 
- *  fails (e.g. port bind error).
- *
- *  @note A copy will be made of the @c MsgFrame and @c IncomingMsgCb objects
- *  before read processing is started. For example, a copy is made each time a TCP
- *  connect is attempted. However, once the read processing is started, no additional
- *  copies will be made. Copying of these objects in an initial state should be
- *  designed to be efficient (and correct), while copying after the initial state
- *  (i.e. once reads are started) does not need the same design rigor.
- */
-  template <class MsgFrame>
-  bool start(const ChannelChangeCb& ccCb, const MsgFrame& msgFrame, 
-             const typename IncomingMsgCb<MsgFrame>::Callback& incMsgCb) {
-
-    if (detail::SockLibResourcePtr p = mResourcePtr.lock()) {
-      return p->start(ccCb, msgFrame, incMsgCb);
-    }
-    throw SockLibResourceException("net_entity.start");
-  }
-
-/**
- *  @brief Start network processing on the associated network resource, but use 
- *  @c SimpleMsgFrame and @c simpleIncomingMsgCb for the @c MsgFrame and
- *  @c IncomingMsgCb parameters.
- *
- *  @param ccCb Same as the three parameter @c start.
- *
- *  @return Same as the three parameter @c start.
- */
-  bool start(const ChannelChangeCb& ccCb) {
-    if (detail::SockLibResourcePtr p = mResourcePtr.lock()) {
-      return p->start(ccCb, SimpleMsgFrame(), simpleIncomingMsgCb);
-    }
-    throw SockLibResourceException("net_entity.start");
-  }
-
-/**
- *  @brief Stop processing on the associated network facility and destroy associated 
- *  resources.
- *
- *  Stopping network processing may involve closing connections, deallocating
- *  resources, and unbinding from ports. It will affect other @c net_entity objects
- *  associated with the same network resource.
- *
- *  A @c ChannelChangeCb will be invoked for each associated TCP connection or UDP resource 
- *  when @c stop is called, with the following values:
- *  1) valid @c OutputChannel corresponding to the connection / resource
- *  2) boost::system::error_code with the value boost::system::errc::operation_canceled
- *  3) 0 / N (count of valid connections or resources for this @c net_entity).
- *
- *  Specifically, a TCP connector and UDP resource will have one @c ChannelChangeCb invocation
- *  with a channel count of 0, while a TCP acceptor will have a @c ChannelChangeCb invocation
- *  for each current connection (and there may be 0 connections), and one additional invocation 
- *  with an empty @c OutputChannel and a channel count of 0.
- *
- *  @note Other method calls made to this object after @c stop is invoked
- *  result in an exception thrown, or undefined behavior. The @c valid method may be 
- *  called to query the state.
- *
- *  @throw @c SockLibException is thrown if the underlying resource is invalid.
+ *  @return @c true if valid net entity association.
  *
  */
   void stop() {
-    if (detail::SockLibResourcePtr p = mResourcePtr.lock()) {
-      p->stop();
-      return;
-    }
-    throw SockLibResourceException("net_entity.stop");
+    auto p = m_eh_wptr.lock();
+    return p ? (p->stop(), true) : false;
   }
-
-  friend bool operator==(const net_entity&, const net_entity&);
-  friend bool operator<(const net_entity&, const net_entity&);
-};
-
 /**
  *  @brief Compare two @c net_entity objects for equality.
  *
- *  See @c OutputChannel notes for equivalence logic (same logic applies here).
+ *  If both @net_entity objects are valid, then a @c std::shared_ptr comparison is made.
+ *  If both @net_entity objects are invalid, @c true is returned (this implies that
+ *  all invalid @c net_entity objects are equivalent). If one is valid and the other invalid,
+ *  @c false is returned.
  *
- *  @return @c true if two @c net_entity objects are pointing to the same internal resource.
- *
- *  @relates net_entity
+ *  @return As described in the comments.
  */
 
-inline bool operator==(const net_entity& lhs, const net_entity& rhs) {
-  detail::SockLibResourcePtr plhs = lhs.mResourcePtr.lock();
-  detail::SockLibResourcePtr prhs = rhs.mResourcePtr.lock();
-  return (plhs && prhs && plhs == prhs) || (!plhs && !prhs);
+  bool operator==(const net_entity<EH>& rhs) const noexcept {
+    auto lp = m_eh_wptr.lock();
+    auto rp = rhs.m_eh_wptr.lock();
+    return (lp && rp && lp == rp) || (!lp && !rp);
+  }
+
+/**
+ *  @brief Compare two @c net_entity objects for ordering purposes.
+ *
+ *  All invalid @c net_entity objects are less than valid ones. When both are valid,
+ *  the @c std::shared_ptr ordering is returned.
+ *
+ *  Specifically, if both @c net_entity objects are invalid @c false is returned. 
+ *  If the left @c net_entity object is invalid and the right is not, @c true is
+ *  returned. If the right @c net_entity is invalid, but the left is not, @c false is 
+ *  returned.
+ *
+ *  @return As described in the comments.
+ */
+  bool operator<(const net_entity<EH>& rhs) const noexcept {
+    auto lp = m_eh_wptr.lock();
+    auto rp = rhs.m_eh_wptr.lock();
+    return (lp && rp && lp < rp) || (!lp && rp);
+  }
+
+};
+
+namespace detail {
+class tcp_connector;
+class tcp_acceptor;
+class udp_entity_io;
 }
 
 /**
- *  @brief Compare two @c net_entity objects for ordering purposes, allowing @c net_entity objects
- *  to be stored in a @c std::map (or other associative container).
- *
- *  See @c OutputChannel notes for ordering logic (same logic applies here).
- *
- *  @return @c true if left @c net_entity less than right @c net_entity according to internal
- *  resource pointer ordering and invalid object ordering as defined in the comments.
+ *  @brief Using declaration for a TCP connector @c net_entity type.
  *
  *  @relates net_entity
  */
-inline bool operator<(const net_entity& lhs, const net_entity& rhs) {
-  detail::SockLibResourcePtr plhs = lhs.mResourcePtr.lock();
-  detail::SockLibResourcePtr prhs = rhs.mResourcePtr.lock();
-  return (plhs && prhs && plhs < prhs) || (!plhs && prhs);
-}
+using tcp_connector_net_entity = net_entity<detail::tcp_connector>;
+
+/**
+ *  @brief Using declaration for a TCP acceptor @c net_entity type.
+ *
+ *  @relates net_entity
+ */
+using tcp_acceptor_net_entity = net_entity<detail::tcp_acceptor>;
+
+/**
+ *  @brief Using declaration for a UDP based @c net_entity type.
+ *
+ *  @relates net_entity
+ */
+using udp_net_entity = net_entity<detail::udp_entity_io>;
 
 } // end net namespace
 } // end chops namespace
