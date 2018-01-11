b86c54a4fbf3a920f129345f9fed269f633df3ec
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-11 11:27:41 -0800

    Initial commit.

---
+++ include/net_ip/worker.hpp
@@ -0,0 +1,67 @@
+/** @file 
+ *
+ *  @ingroup net_ip_module
+ *
+ *  @brief Convenience executor and work guard class.
+ *
+ *  @author Cliff Green
+ *  @date 2018
+ *  @copyright Cliff Green, MIT License
+ *
+ */
+
+#ifndef WORKER_HPP_INCLUDED
+#define WORKER_HPP_INCLUDED
+
+#include <thread>
+
+#include <experimental/io_context>
+#include <experimental/executor>
+
+namespace chops {
+namespace net {
+
+/**
+ *  @brief Convenience class that combines an executor, work guard, and a thread,
+ *  invoking asynchronous operations as per the C++ Networking TS.
+ */
+class worker {
+private:
+  std::experimental::net::io_context m_ioc;
+  std::experimental::net::wk_guard   m_wg;
+  std::thread                        m_run_thr;
+
+public:
+  worker() : m_ioc(), m_wg(make_work_guard(ioc)), m_run_thr() { }
+
+/**
+ *  @brief Provide access to the @c io_context.
+ *
+ *  @return Reference to a @c std::experimental::net::io_context.
+ */
+  std::experimental::net::io_context& get_io_context() { return m_ioc; }
+
+/**
+ *  @brief Start the thread that invokes the underlying asynchronous
+ *  operations.
+ */
+  void start() {
+    m_run_thr = std::thread([&m_ioc] () { ioc.run(); } );
+  }
+
+/**
+ *  @brief Shutdown the executor and join the thread.
+ */
+  void stop() {
+    m_ioc.stop();
+    m_run_thr.join();
+  }
+
+};
+
+
+}  // end net namespace
+}  // end chops namespace
+
+#endif
+
