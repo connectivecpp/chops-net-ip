680e8f7c7f5d3e4c2c83b5f8b24d905261fec392
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-15 13:21:53 -0800

    Rewrote into class with two methods.

--- include/net_ip/endpoints_resolver.hpp
+++ include/net_ip/endpoints_resolver.hpp
@@ -22,12 +22,31 @@
 
 #include <string_view>
 #include <string>
-#include <vector>
 #include <utility> // std::move, std::forward
 
 namespace chops {
 namespace net {
 
+/**
+ *  @brief Convenience class for resolving host names to endpoints suitable for use
+ *  within the Chops Net IP library (or with the C++ Networking TS).
+ *
+ *  This class does not add much functionality above what is already present in the
+ *  Networking TS, but does automate the flags for local resolves.
+ *
+ *  Many times only one endpoint is needed, for example a TCP acceptor local endpoint or a 
+ *  UDP local endpoint. In this case the first entry of a sequence (if there are many) can 
+ *  be used.
+ *
+ *  Name resolving will not be performed if the host name is already in dotted numeric or 
+ *  hexadecimal (ipV6) form, or if the host name is empty (common for when the local
+ *  host IP address is "INADDR_ANY" and used for a TCP acceptor local endpoint). 
+ *
+ *  For the methods taking a function object callback it is the applications responsibility 
+ *  to ensure that the memory context is still valid when the callback is invoked. A typical 
+ *  idiom is to use @c std::shared_from_this as part of the function object callback member.
+ *
+ */
 
 template <typename Protocol>
 class endpoints_resolver {
@@ -36,32 +55,18 @@ private:
 
 public:
 
+/**
+ *  @brief Construct with an @c io_context.
+ *
+ *  @param ioc @c std::experimental::net::io_context used in the resolver.
+ *
+ */
   explicit endpoints_resolver(std::experimental::net::io_context& ioc) : m_resolver(ioc) { }
 
 /**
- *  @brief Create a sequence of endpoints and return them in a function object callback, 
- *  performing name resolving (DNS lookup) as needed.
- *
- *  This function is used in internal Chops Net IP layers, but is available for application 
- *  use if needed.
- *
- *  Many times only one endpoint is needed, for example a TCP acceptor local endpoint or a UDP 
- *  local endpoint. In this case the first entry (if there are many) can be used.
- *
- *  Name resolving will not be performed if the host name is already in dotted numeric or 
- *  hexadecimal (ipV6) form, or if the host name is empty (common for when the local
- *  host IP address is "INADDR_ANY" and used for a TCP acceptor local endpoint). 
+ *  @brief Create a sequence of endpoints and return them in a function object callback.
  *
  *  This function always returns before the function object callback is invoked.
- *
- *  It is the applications responsibility to ensure that the callback context is still valid 
- *  when the callback is invoked. A typical idiom is to use @c std::shared_from_this as part 
- *  of the function object callback member.
- *
- *  @tparam Protocol Either @c std::experimental::net::ip::tcp or 
- *  @c std::experimental::net::ip::udp.
- *
- *  @param ioc @c std::experimental::net::io_context, for asynchronous processing.
  *
  *  @param func Function object which will be invoked when the name resolution completes. The
  *  signature of the callback:
@@ -87,31 +92,32 @@ public:
  *  have a port number of zero.
  *
  */
-template <typename Protocol, typename F>
-void make_endpoints(std::experimental::net::ip::basic_resolver<Protocol>& res, F&& func, bool local,
-      std::string_view host_or_intf_name, std::string_view service_or_port) {
+  template <typename F>
+  void make_endpoints(F&& func, bool local, std::string_view host_or_intf_name, 
+                      std::string_view service_or_port) {
 
-  using namespace std::experimental::net;
+    using namespace std::experimental::net;
 
-  if (local) {
-    res.async_resolve(std::string(host_or_intf_name), std::string(service_or_port), 
-                      ip::resolver_base::flags(ip::resolver_base::passive | 
-                                               ip::resolver_base::address_configured),
-                      std::forward<F>(func));
+    if (local) {
+      m_resolver.async_resolve(std::string(host_or_intf_name), std::string(service_or_port), 
+                        ip::resolver_base::flags(ip::resolver_base::passive | 
+                                                 ip::resolver_base::address_configured),
+                        std::forward<F>(func));
+    }
+    else {
+      m_resolver.async_resolve(std::string(host_or_intf_name), std::string(service_or_port),
+                        std::forward<F>(func));
+    }
+    return;
   }
-  else {
-    res.async_resolve(std::string(host_or_intf_name), std::string(service_or_port),
-                      std::forward<F>(func));
-  }
-  return;
-}
 
 /**
- *  @brief Create a sequence of endpoints and return them in a container.
+ *  @brief Create a sequence of endpoints and return them immediately in a container.
  *
  *  This function performs synchronous (blocking) name resolution instead of asynchronous
- *  resolution. The interface is the same except that a container of endpoints is returned 
- *  instead of a function object callback invocation happening at a later point.
+ *  resolution. The interface is the same as the async method (taking a function object) 
+ *  except that a container of endpoints is returned instead of a function object callback 
+ *  invocation happening at a later point.
  *
  *  @return @c std::experimental::net::ip::basic_resolver_results<Protocol>, where Protocol
  *  is either @c std::experimental::net::ip::tcp or @c std::experimental::net::ip::udp.
@@ -119,18 +125,18 @@ void make_endpoints(std::experimental::net::ip::basic_resolver<Protocol>& res, F
  *  @throw @c std::system_error on failure.
  */
 
-template <typename Protocol>
-auto make_endpoints(std::experimental::net::ip::basic_resolver<Protocol>& res, bool local,
-                    std::string_view host_or_intf_name, std::string_view service_or_port) {
+  auto make_endpoints(bool local, std::string_view host_or_intf_name, std::string_view service_or_port) {
 
-  using namespace std::experimental::net;
+    using namespace std::experimental::net;
 
-  if (local) {
-    ip::resolver_base::flags f (ip::resolver_base::passive | ip::resolver_base::address_configured);
-    return res.resolve(std::string(host_or_intf_name), std::string(service_or_port), f);
+    if (local) {
+      return m_resolver.resolve(std::string(host_or_intf_name), std::string(service_or_port),
+          ip::resolver_base::flags(ip::resolver_base::passive | ip::resolver_base::address_configured));
+    }
+    return m_resolver.resolve(std::string(host_or_intf_name), std::string(service_or_port));
   }
-  return res.resolve(std::string(host_or_intf_name), std::string(service_or_port));
-}
+
+};
 
 }  // end net namespace
 }  // end chops namespace
