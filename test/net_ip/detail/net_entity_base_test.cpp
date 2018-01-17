4edda1ac91ffea663b5f8decd9ce573922846d84
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-17 09:52:42 -0800

    Net entity base now compiles and tests successfully.

--- test/net_ip/detail/net_entity_base_test.cpp
+++ test/net_ip/detail/net_entity_base_test.cpp
@@ -12,146 +12,80 @@
 
 #include "catch.hpp"
 
-#include <experimental/internet> // endpoint declarations
-#include <experimental/io_context>
-
 #include <memory> // std::shared_ptr
 #include <system_error> // std::error_code
 #include <utility> // std::move
-#include <functional> // std::bind
+#include <functional> // std::ref
+#include <cstddef> // std::size_t
 
 #include "net_ip/detail/net_entity_base.hpp"
 #include "net_ip/io_interface.hpp"
 #include "net_ip/net_ip_error.hpp"
 
-#include "utility/repeat.hpp"
-#include "utility/make_byte_array.hpp"
+template<typename IOH>
+struct state_change {
+  bool called = false;
+  std::size_t num;
+  std::error_code err;
+  bool ioh_valid;
+  void operator() (chops::net::io_interface<IOH> ioh, std::error_code e, std::size_t n) {
+    called = true;
+    num = n;
+    err = e;
+    ioh_valid = ioh.is_valid();
+  }
+};
 
 template <typename IOH>
-void net_entity_base_test(chops::const_shared_buffer buf, int num_bufs,
-                    typename IOH::endpoint_type endp) {
+void net_entity_base_test() {
 
-  using namespace std::experimental::net;
-  using namespace std::placeholders;
-  using notifier_type = typename chops::net::detail::io_base<IOH>::entity_notifier_cb;
+  using namespace chops::net;
 
-  REQUIRE (num_bufs > 1);
+  state_change<IOH> state_chg;
+  REQUIRE_FALSE (state_chg.called);
 
-  IOH ioh;
+  detail::net_entity_base<IOH> ne { };
+  REQUIRE_FALSE (ne.is_started());
+  REQUIRE(ne.size() == 0);
 
-  GIVEN ("An io_base and a buf and endpoint") {
+  auto iohp = std::make_shared<IOH>();
 
-    INFO ("Endpoint passed as arg: " << endp);
-    chops::net::detail::io_base<IOH> iobase ( notifier_type(std::bind(&IOH::notify_me, &ioh, _1, _2)) );
-    INFO("Remote endpoint after default construction: " << iobase.get_remote_endp());
+  GIVEN ("A default constructed net_entity_base and a state change object") {
 
-    auto qs = iobase.get_output_queue_stats();
-    REQUIRE (qs.output_queue_size == 0);
-    REQUIRE (qs.bytes_in_output_queue == 0);
-    REQUIRE_FALSE (iobase.is_started());
-    REQUIRE_FALSE (iobase.is_write_in_progress());
-
-    WHEN ("Process_err_code is called") {
-      REQUIRE_FALSE (ioh.notify_called);
-      iobase.process_err_code(std::make_error_code(chops::net::net_ip_errc::message_handler_terminated), 
-                              std::shared_ptr<IOH>());
-      THEN ("the notify_called flag is now true") {
-        REQUIRE (ioh.notify_called);
+    WHEN ("Start is called") {
+      ne.start(std::ref(state_chg));
+      THEN ("net entity base is started, state change called flag is true, other vals ok") {
+        REQUIRE (ne.is_started());
+        REQUIRE (ne.size() == 0);
+        REQUIRE (state_chg.called);
+        REQUIRE (state_chg.num == 0);
+        REQUIRE_FALSE (state_chg.err);
+        REQUIRE_FALSE (state_chg.ioh_valid);
       }
     }
 
-    AND_WHEN ("Start_io_setup is called") {
-      bool ret = iobase.start_io_setup(endp);
-      REQUIRE (ret);
-      THEN ("the started flag is true and write_in_progress flag false") {
-        REQUIRE (iobase.is_started());
-        REQUIRE_FALSE (iobase.is_write_in_progress());
+    AND_WHEN ("Add handler is called") {
+      ne.add_handler(iohp);
+      THEN ("size increases by 1") {
+        REQUIRE (ne.size() == 1);
+      }
+    }
+    AND_WHEN ("Remove handler is called") {
+      ne.remove_handler(iohp);
+      THEN ("size decreases by 1") {
+        REQUIRE (ne.size() == 0);
       }
     }
 
-    AND_WHEN ("Start_io_setup is called twice") {
-      bool ret = iobase.start_io_setup(endp);
-      REQUIRE (ret);
-      ret = iobase.start_io_setup(endp);
-      THEN ("the second call returns false") {
-        REQUIRE_FALSE (ret);
-      }
-    }
-
-    AND_WHEN ("Start_write_setup is called before start_io_setup") {
-      bool ret = iobase.start_write_setup(buf);
-      THEN ("the call returns false") {
-        REQUIRE_FALSE (ret);
-      }
-    }
-
-    AND_WHEN ("Start_write_setup is called after start_io_setup") {
-      bool ret = iobase.start_io_setup(endp);
-      ret = iobase.start_write_setup(buf);
-      THEN ("the call returns true and write_in_progress flag is true and queue size is zero") {
-        REQUIRE (ret);
-        REQUIRE (iobase.is_write_in_progress());
-        REQUIRE (iobase.get_output_queue_stats().output_queue_size == 0);
-      }
-    }
-
-    AND_WHEN ("Start_write_setup is called twice") {
-      bool ret = iobase.start_io_setup(endp);
-      ret = iobase.start_write_setup(buf);
-      ret = iobase.start_write_setup(buf);
-      THEN ("the call returns false and write_in_progress flag is true and queue size is one") {
-        REQUIRE_FALSE (ret);
-        REQUIRE (iobase.is_write_in_progress());
-        REQUIRE (iobase.get_output_queue_stats().output_queue_size == 1);
-      }
-    }
-
-    AND_WHEN ("Start_write_setup is called many times") {
-      bool ret = iobase.start_io_setup(endp);
-      REQUIRE (ret);
-      chops::repeat(num_bufs, [&iobase, &buf, &ret, &endp] () { 
-          ret = iobase.start_write_setup(buf, endp);
-        }
-      );
-      THEN ("all bufs but the first one are queued") {
-        REQUIRE (iobase.is_write_in_progress());
-        REQUIRE (iobase.get_output_queue_stats().output_queue_size == (num_bufs-1));
-      }
-    }
-
-    AND_WHEN ("Start_write_setup is called many times and get_next_element is called 2 less times") {
-      bool ret = iobase.start_io_setup(endp);
-      REQUIRE (ret);
-      chops::repeat(num_bufs, [&iobase, &buf, &ret, &endp] () { 
-          iobase.start_write_setup(buf, endp);
-        }
-      );
-      chops::repeat((num_bufs - 2), [&iobase] () { 
-          iobase.get_next_element();
-        }
-      );
-      THEN ("next get_next_element call returns a val, call after doesn't and write_in_progress is false") {
-
-        auto qs = iobase.get_output_queue_stats();
-        REQUIRE (qs.output_queue_size == 1);
-        REQUIRE (qs.bytes_in_output_queue == buf.size());
-
-        auto e = iobase.get_next_element();
-
-        qs = iobase.get_output_queue_stats();
-        REQUIRE (qs.output_queue_size == 0);
-        REQUIRE (qs.bytes_in_output_queue == 0);
-
-        REQUIRE (iobase.is_write_in_progress());
-
-        REQUIRE (e);
-        REQUIRE (e->first == buf);
-        REQUIRE (e->second == endp);
-
-        auto e2 = iobase.get_next_element();
-        REQUIRE_FALSE (iobase.is_write_in_progress());
-        REQUIRE_FALSE (e2);
-
+    AND_WHEN ("Handlers are added and call state change is called") {
+      ne.start(std::ref(state_chg));
+      ne.add_handler(iohp);
+      ne.add_handler(iohp);
+      ne.call_state_change_cb(std::make_error_code(net_ip_errc::io_handler_stopped), iohp);
+      THEN ("state change internal vals are set correctly") {
+        REQUIRE (state_chg.num == 2);
+        REQUIRE (state_chg.err);
+        REQUIRE (state_chg.ioh_valid);
       }
     }
 
@@ -159,42 +93,20 @@ void net_entity_base_test(chops::const_shared_buffer buf, int num_bufs,
 }
 
 struct tcp_io_mock {
-  bool notify_called = false;
-
-  using endpoint_type = std::experimental::net::ip::tcp::endpoint;
-
-  void notify_me(std::error_code err, std::shared_ptr<tcp_io_mock> p) {
-    notify_called = true;
-  }
-
+  using endpoint_type = int;
+  using socket_type = int;
 };
 
 struct udp_io_mock {
-  bool notify_called = false;
-
-  using endpoint_type = std::experimental::net::ip::udp::endpoint;
-
-  void notify_me(std::error_code err, std::shared_ptr<udp_io_mock> p) {
-    notify_called = true;
-  }
-
+  using endpoint_type = int;
+  using socket_type = int;
 };
 
-SCENARIO ( "Io base test, udp", "[io_base_udp]" ) {
-  using namespace std::experimental::net;
-
-  auto ba = chops::make_byte_array(0x20, 0x21, 0x22, 0x23, 0x24);
-  chops::mutable_shared_buffer mb(ba.data(), ba.size());
-  net_entity_base_test<udp_io_mock>(chops::const_shared_buffer(std::move(mb)), 20,
-                            ip::udp::endpoint(ip::udp::v4(), 1234));
+SCENARIO ( "Net entity base test, udp", "[net_entity_base_udp]" ) {
+  net_entity_base_test<udp_io_mock>();
 }
 
-SCENARIO ( "Io base test, tcp", "[io_base_tcp]" ) {
-  using namespace std::experimental::net;
-
-  auto ba = chops::make_byte_array(0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46);
-  chops::mutable_shared_buffer mb(ba.data(), ba.size());
-  net_entity_base_test<tcp_io_mock>(chops::const_shared_buffer(std::move(mb)), 40,
-                            ip::tcp::endpoint(ip::tcp::v6(), 9876));
+SCENARIO ( "Net entity base test, tcp", "[net_entity_base_tcp]" ) {
+  net_entity_base_test<tcp_io_mock>();
 }
 
