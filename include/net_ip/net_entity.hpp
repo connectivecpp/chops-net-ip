e2faf9cd0b5fc9d92de18724f6215e40a17efc08
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-01-18 09:29:38 -0800

    Adding a second state change callback.

--- include/net_ip/net_entity.hpp
+++ include/net_ip/net_entity.hpp
@@ -82,7 +82,6 @@ public:
 /**
  *  @brief Construct with a shared weak pointer to an internal net entity, this is an
  *  internal constructor only and not to be used by application code.
- *
  */
 
   explicit net_entity (std::weak_ptr<ET> p) noexcept : m_eh_wptr(p) { }
@@ -112,90 +111,90 @@ public:
 
 /**
  *  @brief Start network processing on the associated net entity with the application
- *  providing a state change function object callback.
+ *  providing state change function object callbacks.
  *
  *  Once a net entity (TCP acceptor, TCP connector, UDP entity) is created through 
- *  a @c net_ip @c make method, @c start is then called, and this is when
- *  local port binding and other processing (e.g. TCP listen, TCP connect) occurs.
+ *  a @c net_ip @c make method, the application can call this method. At this 
+ *  time local port binding and other processing (e.g. TCP listen, TCP connect) occurs.
  *
  *  Input and output processing does not start until the @c io_interface @c start_io
  *  method is called.
  *
- *  The application provides a state change function object callback which
- *  allows the internal net entity to notify the application when one of the 
- *  following occurs: 1) a TCP connect succeeds, or 2) a TCP accept succeeds, or
- *  3) a UDP entity is ready.
+ *  The application provides two state change function object callbacks:
  *
- *  The same state change callback is invoked when processing is stopped due
- *  to an error or connection close or application close.
+ *  1) An "IO ready" callback which is invoked when a TCP connection is created or 
+ *  a UDP entity becomes ready. An @c io_interface object is provided to the callback 
+ *  which allows IO processing to commence through the @c start_io method call.
  *
- *  @c start can be called, followed by @c stop, followed by @c start, etc.
- *  This may be useful (for example) for a TCP connector that needs to 
- *  reconnect after a connection has been lost.
+ *  2) A "stop" callback which is invoked when a TCP connection is shutdown or taken 
+ *  down through an error, or a TCP or UDP entity encounters an error and cannot 
+ *  continue.
  *
- *  @param state_chg A function object callback with the following signature:
+ *  The @c start method call can be followed by a @c stop call, followed by @c start, 
+ *  etc. This may be useful (for example) for a TCP connector that needs to reconnect 
+ *  after a connection has been lost.
+ *
+ *  @param io_ready_func A function object callback with the following signature:
  *
  *  @code
- *    // TCP io:
+ *    // TCP:
+ *    void (chops::net::tcp_io_interface, std::size_t);
+ *    // UDP:
+ *    void (chops::net::udp_io_interface, std::size_t);
+ *  @endcode
+ *
+ *  The parameters are as follows:
+ *
+ *  1) An @c io_interface object providing @c start_io and @c stop_io access to the 
+ *  underlying IO handler.
+ *
+ *  2) A count of the underlying IO handlers associated with this net entity. For 
+ *  a TCP connector or a UDP entity the number is 1, and for a TCP acceptor the number is 
+ *  1 to N, depending on the number of accepted connections.
+ *
+ *  @param stop_func A function object callback with the following signature:
+ *
+ *  @code
+ *    // TCP:
  *    void (chops::net::tcp_io_interface, std::error_code, std::size_t);
- *    // UDP io:
+ *    // UDP:
  *    void (chops::net::udp_io_interface, std::error_code, std::size_t);
  *  @endcode
  *
  *  The parameters are as follows:
  *
- *  1) provides @c io_interface access (@c start_io, @c stop_io, etc) to the 
- *  underlying IO handler
+ *  1) An @c io_interface object, which may be empty (i.e invalid, where @c is_valid 
+ *  returns @c false), depending on the context of the error. If non-empty (valid),
+ *  this can be correlated with the @c io_interface provided in the @c io_ready_func 
+ *  callback. No methods on the @c io_interface object should be called, as the 
+ *  underlying handler is being destructed.
  *
- *  2) error code, which may be zero (no error) or contain a system error or 
- *  a Chops Net IP error.
+ *  2) The error code associated with the shutdown. There are error codes associated 
+ *  with a graceful shutdown as well as network or system errors.
  *
- *  3) number of underlying IO handlers associated with this net entity; for 
- *  a TCP connector or a UDP entity the number is either 0 or 1; for a TCP 
- *  acceptor the number is 0 to N, depending on the number of accepted 
- *  connections
+ *  3) A count of the underlying IO handlers associated with this net entity, either 
+ *  0 for a TCP connector or UDP entity, or 0 to N for a TCP acceptor.
  *
- *  The state change function object callback is always invoked at least once
- *  after @c start and before any network processing starts, allowing initialization
- *  of application states (if needed). The values will be:
- *
- *  1) a default constructed @c io_interface (@c is_valid() == @c false) 
- *
- *  2) zero (non-error) std::error_code 
- *
- *  3) 0 (i.e. no connections or binds have occurred)
+ *  The @c stop_func callback may be invoked in contexts other than an IO error - for
+ *  example, if a TCP acceptor or UDP entity cannot bind to a local port, a system error 
+ *  code will be provided.
  *
  *  @return @c true if valid net entity association.
  *
  */
-  template <typename F>
-  bool start(F&& state_chg) {
+  template <typename R, typename S>
+  bool start(R&& io_ready_func, S&& stop_func ) {
     auto p = m_eh_wptr.lock();
-    return p ? (p->start(std::forward<F>(state_chg)), true) : false;
+    return p ? (p->start(std::forward<R>(io_ready_func), std::forward<S>(stop_func)), true) : false;
   }
 
 /**
  *  @brief Stop network processing on the associated net entity after calling @c stop_io on
- *  each associated IO handler, invoke the application provided state change function object
- *  callback.
+ *  each associated IO handler.
  *
  *  Stopping the network processing may involve closing connections, deallocating
- *  resources, and unbinding from ports. 
- *
- *  The state change function object callback will be invoked, possibly multiple times,
- *  with the following values:
- *
- *  1) a valid @c io_interface corresponding to the @c io_interface provided after @c start
- *
- *  2) std::error_code with the value chops::net::net_ip_errc::io_handler_stopped
- *
- *  3) 0 / N (count of valid IO handlers or resources for this @c net_entity)
- *
- *  Specifically, a TCP connector and a UDP net entity will have one state change callback
- *  invocation with a IO handler count of 0, while a TCP acceptor will have a state change
- *  callback invocation for each current connection (and there may be 0 connections), and one 
- *  additional invocation with a default constructed (i.e. invalid) @c io_interface and a 
- *  IO handler count of 0.
+ *  resources, unbinding from ports, and invoking application provided state change function 
+ *  object callbacks. 
  *
  *  @return @c true if valid net entity association.
  *
