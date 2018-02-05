fcadf822752643c284f72158a367523df428731b
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2018-02-04 16:09:46 -0800

    Initial commit.

---
+++ include/net_ip/component/simple_variable_len_msg_frame.hpp
@@ -0,0 +1,59 @@
+/** @file 
+ *
+ *  @ingroup net_ip_module
+ *
+ *  @brief @c io_interface class template and related functionality.
+ *
+ *  @author Cliff Green
+ *  @date 2017, 2018
+ *  @copyright Cliff Green, MIT License
+ *
+ */
+
+#ifndef SIMPLE_VARIABLE_LEN_MSG_FRAME_INCLUDED
+#define SIMPLE_VARIABLE_LEN_MSG_FRAME_INCLUDED
+
+#include <cstddef> // std::size_t, std::byte
+
+#include <experimental/buffer>
+
+namespace chops {
+namespace net {
+
+/**
+ *  @brief Signature for a variable length message header decoder function.
+ *
+ *  Given a buffer of @c std::bytes corresponding to a variable length message header, 
+ *  decode the header and return the length of the message body.
+ *
+ *  @param ptr A pointer to the beginning of the variable len message header.
+ *
+ *  @param sz The size of the header buffer, which should be the same for every call 
+ *  and match the parameter given to the @c io_interface @c start_io call.
+ *
+ *  @relates make_simple_variable_len_msg_frame
+ */
+using hdr_decoder_func = std::size_t (*)(const std::byte* ptr, std::size_t sz);
+
+/**
+ *  @brief Create a message frame function object for the use case of a variable len 
+ *  message composed of a single header and a single body, where the header decoding 
+ *  function is supplied by the application.
+ *
+ */
+inline auto make_simple_variable_len_msg_frame(hdr_decoder_func func) {
+  bool hdr_processed = false;
+  return [hdr_processed, func] 
+      (std::experimental::net::mutable_buffer buf) mutable -> std::size_t {
+    return hdr_processed ? 
+        (hdr_processed = false, 0) :
+        (hdr_processed = true, func(static_cast<const std::byte*>(buf.data()), buf.size()));
+  };
+}
+
+
+} // end net namespace
+} // end chops namespace
+
+#endif
+
