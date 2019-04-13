a91408ba8b89d913648553a9d501ba0a1c6fc007
Thurman Gillespy <thurman.gillespy@comcast.net>
2019-04-13 13:13:57 -0700

    first working version, with extended version of send_to_all::send

--- example/multichat_server_demo.cpp
+++ example/multichat_server_demo.cpp
@@ -28,6 +28,7 @@ multichat_server_demo.cpp -lpthread -o server
 #include <string>
 #include <chrono>
 #include <thread>
+#include <utility> // std::ref
 #include <cassert>
 
 #include "net_ip/net_ip.hpp"
@@ -43,148 +44,95 @@ using endpoint = asio::ip::tcp::endpoint;
 
 // process command line args, set ip_addr, port, param as needed
 bool process_args(int argc, char *argv[], std::string &ip_addr,
-                  std::string &port, std::string &param)
-{
-    const std::string PORT = "5001";
-    const std::string LOCAL_LOOP = "127.0.0.1";
-    const std::string USAGE =
-        "usage:\n"
-        "  ./mchat [-h]  print usage info\n"
-        "  ./mchat -accept [port]    multichat server\n"
-        "  ./mchat -connect [IP address/hostname] [port]    multichat client\n";
-    "    default port = " + PORT + "\n";
-    const std::string HELP = "-h";
-    const std::string EMPTY = "";
+                  std::string &port, std::string &param) {
+   const std::string PORT = "5001";
+   const std::string LOCAL_LOOP = "127.0.0.1";
+   const std::string USAGE =
+       "usage:\n"
+       "  ./chat_server [-h]  [port]\n"
+       "     -h   print usage\n"
+       "      default port = " + PORT + "\n"
+       "      server IP address = " + LOCAL_LOOP + " (local loop)";
 
-    // set default values
-    ip_addr = LOCAL_LOOP;
-    port = PORT;
+   const std::string HELP = "-h";
 
-    std::cout << USAGE << std::endl;
+   // set default values
+   ip_addr = LOCAL_LOOP;
+   port = PORT;
 
-    return EXIT_SUCCESS;
+   if (argc > 2 || (argc == 2 && argv[1] == HELP)) {
+      std::cout << USAGE << std::endl;
+      return EXIT_FAILURE;
+   }
+
+   if (argc == 2) {
+      port = argv[1];
+   }
+
+   return EXIT_SUCCESS;
 }
-
-struct io_handler_mock
-{
-    using socket_type = int;
-    using endpoint_type = asio::ip::udp::endpoint;
-
-    socket_type sock = 3;
-    bool started = false;
-    constexpr static std::size_t qs_base = 42;
-
-    bool is_io_started() const { return started; }
-
-    socket_type &get_socket() { return sock; }
-
-    chops::net::output_queue_stats get_output_queue_stats() const
-    {
-        return chops::net::output_queue_stats{qs_base, qs_base + 1};
-    }
-    bool send_called = false;
-
-    void send(chops::const_shared_buffer) { send_called = true; }
-    void send(chops::const_shared_buffer, const endpoint_type &) { send_called = true; }
-
-    bool mf_sio_called = false;
-    bool delim_sio_called = false;
-    bool rd_sio_called = false;
-    bool rd_endp_sio_called = false;
-    bool send_sio_called = false;
-    bool send_endp_sio_called = false;
-
-    template <typename MH, typename MF>
-    bool start_io(std::size_t, MH &&, MF &&)
-    {
-        return started ? false : started = true, mf_sio_called = true, true;
-    }
-
-    template <typename MH>
-    bool start_io(std::string_view, MH &&)
-    {
-        return started ? false : started = true, delim_sio_called = true, true;
-    }
-
-    template <typename MH>
-    bool start_io(std::size_t, MH &&)
-    {
-        return started ? false : started = true, rd_sio_called = true, true;
-    }
-
-    template <typename MH>
-    bool start_io(const endpoint_type &, std::size_t, MH &&)
-    {
-        return started ? false : started = true, rd_endp_sio_called = true, true;
-    }
-
-    bool start_io()
-    {
-        return started ? false : started = true, send_sio_called = true, true;
-    }
-
-    bool start_io(const endpoint_type &)
-    {
-        return started ? false : started = true, send_endp_sio_called = true, true;
-    }
-
-    bool stop_io()
-    {
-        return started ? started = false, true : false;
-    }
-};
 
 int main(int argc, char *argv[])
 {
-    const std::string LOCAL = "[local]  ";
-    const std::string SYSTEM = "[system] ";
-    const std::string SERVER = "[server] ";
-    const std::string DELIM = "\a"; // alert (bell)
-    const std::string NO_CONNECTION = "error: no connection" + DELIM;
-    const std::string ABORT = "abort: too many errors";
-    const std::string WAIT_CONNECT = "waiting for connection..." + DELIM;
-    std::string ip_addr;
-    std::string port;
-    std::string param;
+   const std::string LOCAL = "[local]  ";
+   const std::string SYSTEM = "[system] ";
+   const std::string SERVER = "[server] ";
+   const std::string DELIM = "\a"; // alert (bell)
+   const std::string NO_CONNECTION = "error: no connection" + DELIM;
+   const std::string ABORT = "abort: too many errors";
+   const std::string WAIT_CONNECT = "waiting for connection..." + DELIM;
+   std::string ip_addr;
+   std::string port;
+   std::string param;
+   bool finished = false;
 
-    // work guard - handles @c std::thread and @c asio::io_context management
-    chops::net::worker wk;
-    wk.start();
+   if (process_args(argc, argv, ip_addr, port, param) == EXIT_FAILURE) {
+      return EXIT_FAILURE;
+   }
+   // DEBUG
+   // std::cout << ip_addr << ":" << port << std::endl;
+   // return 0;
 
-    chops::net::send_to_all<io_handler_mock> sta;
+   // work guard - handles @c std::thread and @c asio::io_context management
+   chops::net::worker wk;
+   wk.start();
 
-    if (process_args(argc, argv, ip_addr, port, param) == EXIT_FAILURE)
-    {
-        return EXIT_FAILURE;
-    }
+   chops::net::send_to_all<chops::net::tcp_io> sta;
+   /* lamda handlers */
+   // receive text from client, send out to others
+   auto msg_hndlr = [&](const_buf buf, io_interface iof, endpoint ep) {
+      sta.send(buf.data(), buf.size(), iof);
+      return true;
+   };
 
-    /* lamda handlers */
-    // receive text from client, send out to others
-    auto msg_hndlr = [&](const_buf buf, io_interface iof, endpoint ep) {
+   auto io_state_chng_hndlr = [&](io_interface iof, std::size_t n, bool flag) {
+      // add to or remove from list
+      sta(iof, n, flag);
+      if (flag) {
+         iof.start_io(DELIM, msg_hndlr);
+      }
+   };
 
-    };
+   auto err_func = [&](io_interface iof, std::error_code err) {
+      std::cerr << err << " " << err.message() << std::endl;
+   };
 
-    auto io_state_chng_hndlr = [&](io_interface iof, std::size_t n, bool flag) {
-        // call start_io, add to or remove from list
-        iof.start_io(DELIM, msg_hndlr);
-        sta.add_io_interface(iof);
-    };
+   // create @c net_ip instance
+   chops::net::net_ip server(wk.get_io_context());
+   // make @c tcp_acceptor, ruitn @c network_entity
+   auto net_entity = server.make_tcp_acceptor(port.c_str(), ip_addr.c_str());
+   // start network entity, emplace handlers
+   net_entity.start(io_state_chng_hndlr, err_func);
 
-    auto err_func = [&](io_interface iof, std::error_code err) {
+   while (!finished) {
+      std::string s;
+      std::cout << "press any key to exit" << std::endl;
+      getline(std::cin, s);
+      finished = true;
+   }
+   
+   wk.stop();
 
-    };
 
-
-    // create @c net_ip instance
-    chops::net::net_ip server(wk.get_io_context());
-    // make @c tcp_acceptor, ruitn @c network_entity
-    auto net_entity = server.make_tcp_acceptor(port.c_str(), ip_addr.c_str());
-    // start network entity, emplace handlers
-    net_entity.start(io_state_chng_hndlr, err_func);
-
-    while (!finished) {
-
-    }
-    
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
