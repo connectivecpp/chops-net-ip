27dd335903a92c058c521549120f44b79d5525f7
Thurman Gillespy <thurman.gillespy@comcast.net>
2019-04-29 20:16:26 -0700

    changed ERROR to ERROR_MSG

--- example/simple_chat_demo.cpp
+++ example/simple_chat_demo.cpp
@@ -178,7 +178,7 @@ bool process_args(int argc, char* const argv[], std::string& ip_addr,
 int main(int argc, char* argv[]) {
     const std::string LOCAL = "[local]  ";
     const std::string SYSTEM = "[system] ";
-    const std::string ERROR = "[error]  ";
+    const std::string ERROR_MSG = "[error]  ";
     const std::string DELIM = "\a"; // alert (bell)
     const std::string NO_CONNECTION = "no connection..." + DELIM;
     const std::string ABORT = "abort: too many errors";
@@ -280,7 +280,7 @@ int main(int argc, char* argv[]) {
                 std::string err_text = err.category().name();
                 err_text += ": " + std::to_string(err.value()) + ", " +
                 err.message() + DELIM;
-                screen.insert_scroll_line(err_text, ERROR);
+                screen.insert_scroll_line(err_text, ERROR_MSG);
                 screen.draw_screen();
             }
         };
