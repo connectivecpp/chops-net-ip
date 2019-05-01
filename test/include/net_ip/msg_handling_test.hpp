9ecdd4ae8ba1a511baba0de9656c26eb3beef4ec
Cliff Green <softwarelibre@codewrangler.net>
2019-05-01 14:19:55 -0700

    Unit test reorg and revamp.

---
+++ test/include/net_ip/msg_handling_test.hpp
@@ -0,0 +1,181 @@
+/** @file
+ *
+ *  @ingroup test_module
+ *
+ *  @brief Declarations and implementations for shared test code dealing with test
+ *  message building and handler function object classes.
+ *
+ *  The general Chops Net IP test strategy is to have message senders and message 
+ *  receivers, with a flag specifying whether the receiver is to loop back the
+ *  messages. For TCP it is independent of whether the sender or receiver is an 
+ *  acceptor or connector, although most tests have the connector being a sender. In 
+ *  the test routines, coordination is typically needed to know when a connection has 
+ *  been made or sender / receiver is ready so that message flow can start. At the 
+ *  higher layers, the Chops Net IP library facilities provide connection state
+ *  change function object callbacks.
+ *
+ *  When the message flow is finished, an empty body message is sent to the receiver
+ *  (and looped back if the reply flag is set), which signals an "end of message 
+ *  flow" condition. The looped back empty message may not arrive back to the 
+ *  sender since connections or handlers are in the process of being taken down.
+ *
+ *  @author Cliff Green
+ *
+ *  Copyright (c) 2017-2019 by Cliff Green
+ *
+ *  Distributed under the Boost Software License, Version 1.0. 
+ *  (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#ifndef MSG_HANDLING_TEST_HPP_INCLUDED
+#define MSG_HANDLING_TEST_HPP_INCLUDED
+
+#include <string_view>
+#include <cstddef> // std::size_t, std::byte
+#include <cstdint> // std::uint16_t
+#include <vector>
+#include <utility> // std::forward, std::move
+#include <atomic>
+
+#include <cassert>
+#include <limits>
+
+#include "asio/buffer.hpp"
+#include "asio/ip/udp.hpp" // ip::udp::endpoint
+#include "asio/ip/address.hpp" // make_address
+
+#include "utility/repeat.hpp"
+#include "utility/make_byte_array.hpp"
+
+#include "marshall/extract_append.hpp"
+#include "marshall/shared_buffer.hpp"
+
+#include "net_ip/io_interface.hpp"
+#include "net_ip/io_output.hpp"
+
+namespace chops {
+namespace test {
+
+inline std::size_t decode_variable_len_msg_hdr(const std::byte* buf_ptr, std::size_t sz) {
+  assert (sz == 2);
+  return extract_val<std::uint16_t>(buf_ptr);
+}
+
+inline chops::mutable_shared_buffer make_body_buf(std::string_view pre, 
+                                                  char body_char, 
+                                                  std::size_t num_body_chars) {
+  chops::mutable_shared_buffer buf(pre.data(), pre.size());
+  std::string body(num_body_chars, body_char);
+  return buf.append(body.data(), body.size());
+}
+
+inline chops::const_shared_buffer make_variable_len_msg(const chops::mutable_shared_buffer& body) {
+  assert(body.size() < std::numeric_limits<std::uint16_t>::max());
+  std::byte hdr[2];
+  auto sz = append_val(hdr, static_cast<std::uint16_t>(body.size()));
+  chops::mutable_shared_buffer msg(hdr, 2);
+  return chops::const_shared_buffer(std::move(msg.append(body.data(), body.size())));
+}
+
+inline chops::const_shared_buffer make_cr_lf_text_msg(const chops::mutable_shared_buffer& body) {
+  chops::mutable_shared_buffer msg(body.data(), body.size());
+  auto ba = chops::make_byte_array(0x0D, 0x0A); // CR, LF
+  return chops::const_shared_buffer(std::move(msg.append(ba.data(), ba.size())));
+}
+
+inline chops::const_shared_buffer make_lf_text_msg(const chops::mutable_shared_buffer& body) {
+  chops::mutable_shared_buffer msg(body.data(), body.size());
+  auto ba = chops::make_byte_array(0x0A); // LF
+  return chops::const_shared_buffer(std::move(msg.append(ba.data(), ba.size())));
+}
+
+template <typename F>
+chops::const_shared_buffer make_empty_body_msg(F&& func) {
+  return func( chops::mutable_shared_buffer{ } );
+}
+
+inline auto make_empty_variable_len_msg() { return make_empty_body_msg(make_variable_len_msg); }
+inline auto make_empty_cr_lf_text_msg() { return make_empty_body_msg(make_cr_lf_text_msg); }
+inline auto make_empty_lf_text_msg() { return make_empty_body_msg(make_lf_text_msg); }
+
+using vec_buf = std::vector<chops::const_shared_buffer>;
+
+template <typename F>
+vec_buf make_msg_vec(F&& func, std::string_view pre, char body_char, int num_msgs) {
+  vec_buf vec;
+  chops::repeat(num_msgs, [&vec, f = std::forward<F>(func), pre, body_char] (int i) {
+      vec.push_back (f(make_body_buf(pre, body_char, i+1)));
+    }
+  );
+  return vec;
+}
+
+using test_counter = std::atomic_size_t;
+
+template <typename IOT>
+struct msg_hdlr {
+  using endp_type = typename IOT::endpoint_type;
+  using const_buf = asio::const_buffer;
+
+  bool               reply;
+  test_counter&      cnt;
+
+  msg_hdlr(bool rep, test_counter& c) : reply(rep), cnt(c) { }
+
+  bool operator()(const_buf buf, chops::net::basic_io_output<IOT> io_out, endp_type endp) {
+    chops::const_shared_buffer sh_buf(buf.data(), buf.size());
+    if (sh_buf.size() > 2) { // not a shutdown message
+      ++cnt;
+      if (reply) {
+        bool r = io_out.send(sh_buf, endp);
+        assert(r);
+      }
+      return true;
+    }
+    if (reply) {
+      // may not make it back to sender, depending on TCP connection or UDP reliability
+      bool r = io_out.send(sh_buf, endp);
+      assert(r);
+    }
+    return false;
+  }
+
+};
+
+using tcp_msg_hdlr = msg_hdlr<chops::net::tcp_io>;
+using udp_msg_hdlr = msg_hdlr<chops::net::udp_io>;
+
+inline bool tcp_start_io (chops::net::tcp_io_interface io, bool reply, 
+                   std::string_view delim, test_counter& cnt) {
+  if (delim.empty()) {
+    return io.start_io(2, tcp_msg_hdlr(reply, cnt), decode_variable_len_msg_hdr);
+  }
+  return io.start_io(delim, tcp_msg_hdlr(reply, cnt));
+}
+
+constexpr int udp_max_buf_size = 65507;
+
+inline bool udp_start_io (chops::net::udp_io_interface io, bool reply, test_counter& cnt) {
+  return io.start_io(udp_max_buf_size, udp_msg_hdlr(reply, cnt));
+}
+
+inline bool udp_start_io (chops::net::udp_io_interface io, bool receiving, test_counter& cnt,
+                          const asio::ip::udp::endpoint& remote_endp) {
+  if (receiving) {
+    return io.start_io(remote_endp, udp_max_buf_size, udp_msg_hdlr(false, cnt));
+  }
+  return io.start_io(remote_endp);
+}
+
+asio::ip::udp::endpoint make_udp_endpoint(const char* addr, int port_num) {
+  return asio::ip::udp::endpoint(asio::ip::make_address(addr),
+                           static_cast<unsigned short>(port_num));
+}
+
+
+} // end namespace test
+} // end namespace chops
+
+#endif
+
---
+++ test/net_ip/msg_handling_test.cpp
@@ -0,0 +1,260 @@
+/** @file
+ *
+ *  @ingroup test_module
+ *
+ *  @brief Test the message handling utility test code shared between @c net_ip tests.
+ *
+ *  The body of a msg is constructed of a preamble followed by a repeated 
+ *  char. There are three forms of messages:
+ *  1. Variable len: header is 16 bit big endian integer containing length of body
+ *  2. Text, CR LF: body is followed by Ascii CR and LF chars
+ *  3. Text, LF: body is followed by Ascii LF char
+ *
+ *  @author Cliff Green
+ *
+ *  Copyright (c) 2018-2019 by Cliff Green
+ *
+ *  Distributed under the Boost Software License, Version 1.0. 
+ *  (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ *
+ */
+
+#include "catch2/catch.hpp"
+
+#include <string_view>
+#include <cstddef> // std::size_t, std::byte
+#include <cstdint> // std::uint16_t
+#include <vector>
+#include <algorithm>
+#include <memory> // std::shared_ptr, std::make_shared
+#include <future>
+
+#include "asio/ip/udp.hpp"
+#include "asio/buffer.hpp"
+
+#include "utility/make_byte_array.hpp"
+#include "utility/shared_buffer.hpp"
+
+#include "net_ip/basic_io_interface.hpp"
+#include "net_ip/io_interface.hpp"
+
+#include "net_ip/msg_handling_test.hpp"
+
+void make_msg_test() {
+  using namespace chops::test;
+
+  GIVEN ("A body consisting of a preamble and a char to repeat") {
+
+    auto body = make_body_buf("HappyNewYear!", 'Q', 10);
+    REQUIRE (body.size() == 23);
+
+    WHEN ("make_variable_len_msg is called") {
+      auto msg = make_variable_len_msg(body);
+      THEN ("the correct header is prepended") {
+        REQUIRE (msg.size() == 25); // full size of msg
+        REQUIRE (*(msg.data()+0) == std::byte(0x00));
+        REQUIRE (*(msg.data()+1) == std::byte(0x17)); // header is 16 bits, value 23 in big endian
+        REQUIRE (*(msg.data()+2) == std::byte(0x48)); // 'H' in ascii
+        REQUIRE (*(msg.data()+3) == std::byte(0x61)); // 'a' in ascii
+        REQUIRE (*(msg.data()+15) == std::byte(0x51)); // 'Q' in ascii
+        REQUIRE (*(msg.data()+16) == std::byte(0x51)); // 'Q' in ascii
+      }
+    }
+
+    AND_WHEN ("make_cr_lf_text_msg is called") {
+      auto msg = make_cr_lf_text_msg(body);
+      THEN ("CR and LF are appended") {
+        REQUIRE (msg.size() == 25); // full size of msg
+        REQUIRE (*(msg.data()+0) == std::byte(0x48)); // 'H' in ascii
+        REQUIRE (*(msg.data()+1) == std::byte(0x61)); // 'a' in ascii
+        REQUIRE (*(msg.data()+13) == std::byte(0x51)); // 'Q' in ascii
+        REQUIRE (*(msg.data()+14) == std::byte(0x51)); // 'Q' in ascii
+        REQUIRE (*(msg.data()+23) == std::byte(0x0D)); // CR in ascii
+        REQUIRE (*(msg.data()+24) == std::byte(0x0A)); // LF in ascii
+      }
+    }
+
+    AND_WHEN ("make_lf_text_msg is called") {
+      auto msg = make_lf_text_msg(body);
+      THEN ("LF is appended") {
+        REQUIRE (msg.size() == 24); // full size of msg
+        REQUIRE (*(msg.data()+0) == std::byte(0x48)); // 'H' in ascii
+        REQUIRE (*(msg.data()+1) == std::byte(0x61)); // 'a' in ascii
+        REQUIRE (*(msg.data()+13) == std::byte(0x51)); // 'Q' in ascii
+        REQUIRE (*(msg.data()+14) == std::byte(0x51)); // 'Q' in ascii
+        REQUIRE (*(msg.data()+23) == std::byte(0x0A)); // LF in ascii
+      }
+    }
+
+    AND_WHEN ("a larger buffer is passed to make_variable_len_msg") {
+      auto body = make_body_buf("HappyNewYear!", 'Q', 500);
+      REQUIRE (body.size() == 513);
+
+      auto msg = make_variable_len_msg(body);
+
+      THEN ("the correct header is prepended") {
+        REQUIRE (msg.size() == 515); // full size of msg
+        REQUIRE (*(msg.data()+0) == std::byte(0x02));
+        REQUIRE (*(msg.data()+1) == std::byte(0x01)); // header is 16 bits, value 513 in big endian
+      }
+    }
+
+  } // end given
+}
+
+template <typename F>
+void make_msg_vec_test(F&& f) {
+  using namespace chops::test;
+
+  GIVEN ("A preamble and a char to repeat") {
+    auto empty = make_empty_body_msg(f);
+    int delta = empty.size();
+    REQUIRE (delta <= 2);
+    WHEN ("make_msg_vec is called") {
+      auto vb = make_msg_vec(f, "Good tea!", 'Z', 20);
+      THEN ("a vector of buffers is returned") {
+        REQUIRE (vb.size() == 20);
+        chops::repeat(20, [&vb, delta] (int i) { REQUIRE (vb[i].size() == (i+10+delta)); } );
+      }
+    }
+  } // end given
+}
+
+template <typename F>
+std::size_t msg_hdlr_stress_test(F&& f, std::string_view pre, char body_char, int num_msgs) {
+  using namespace chops::test;
+
+  auto msgs = make_msg_vec(f, pre, body_char, num_msgs);
+  auto empty = make_empty_body_msg(f);
+
+  auto iohp = std::make_shared<io_handler_mock>();
+  asio::ip::udp::endpoint endp { };
+
+  test_counter cnt(0);
+  msg_hdlr<io_handler_mock> mh(false, cnt);
+
+  int m = 0;
+  for (auto i : msgs) {
+    auto ret = mh(asio::const_buffer(i.data(), i.size()), io_interface_mock(iohp), endp);
+    if (++m % 1000 == 0) {
+      REQUIRE(ret);
+    }
+  }
+  REQUIRE_FALSE(mh(asio::const_buffer(empty.data(), empty.size()), io_interface_mock(iohp), endp));
+
+  return cnt.load();
+
+}
+
+std::size_t msg_hdlr_stress_test_variable_len_msg(std::string_view pre, char body_char, int num_msgs) {
+  return msg_hdlr_stress_test(chops::test::make_variable_len_msg, pre, body_char, num_msgs);
+}
+std::size_t msg_hdlr_stress_test_cr_lf_text_msg(std::string_view pre, char body_char, int num_msgs) {
+  return msg_hdlr_stress_test(chops::test::make_cr_lf_text_msg, pre, body_char, num_msgs);
+}
+std::size_t msg_hdlr_stress_test_lf_text_msg(std::string_view pre, char body_char, int num_msgs) {
+  return msg_hdlr_stress_test(chops::test::make_lf_text_msg, pre, body_char, num_msgs);
+}
+
+SCENARIO ( "Message handling shared test utility, make msg",
+           "[msg_handling] [make_msg]" ) {
+
+  make_msg_test();
+
+}
+
+SCENARIO ( "Message handling shared test utility, make msg vec",
+           "[msg_handling] [make_msg_vec]" ) {
+  using namespace chops::test;
+
+  make_msg_vec_test(make_variable_len_msg);
+  make_msg_vec_test(make_cr_lf_text_msg);
+  make_msg_vec_test(make_lf_text_msg);
+
+}
+
+SCENARIO ( "Message handling shared test utility, decode variable len msg header",
+           "[msg_handling] [decode_variable_len_msg]" ) {
+  using namespace chops::test;
+
+  auto ba = chops::make_byte_array(0x02, 0x01); // 513 in big endian
+
+  GIVEN ("A two byte buffer that is a variable len msg header") {
+    WHEN ("the decode variable len msg hdr function is called") {
+      THEN ("the correct length is returned") {
+        REQUIRE(decode_variable_len_msg_hdr(ba.data(), 2) == 513);
+      }
+    }
+    AND_WHEN ("a simple variable len msg frame is constructed") {
+      asio::mutable_buffer buf(ba.data(), ba.size());
+      auto mf = chops::net::make_simple_variable_len_msg_frame(decode_variable_len_msg_hdr);
+      THEN ("the returned length toggles between the decoded length and zero") {
+        REQUIRE(mf(buf) == 513);
+        REQUIRE(mf(buf) == 0);
+        REQUIRE(mf(buf) == 513);
+        REQUIRE(mf(buf) == 0);
+      }
+    }
+  } // end given
+}
+
+SCENARIO ( "Message handling shared test utility, msg hdlr function object",
+           "[msg_handling] [msg_hdlr]" ) {
+  using namespace chops::test;
+
+  auto iohp = std::make_shared<io_handler_mock>();
+  REQUIRE_FALSE(iohp->send_called);
+  asio::ip::udp::endpoint endp { };
+
+  auto msg = make_variable_len_msg(make_body_buf("Bah, humbug!", 'T', 4));
+  auto empty = make_empty_variable_len_msg();
+
+  GIVEN ("A mock io handler, a msg with a body, and an empty body msg") {
+
+    WHEN ("a msg hdlr is created with reply true and send is called") {
+      test_counter cnt(0);
+      msg_hdlr<io_handler_mock> mh(true, cnt);
+      THEN ("the shutdown message is handled correctly and count is correct") {
+        REQUIRE(mh(asio::const_buffer(msg.data(), msg.size()), io_interface_mock(iohp), endp));
+        REQUIRE(iohp->send_called);
+        REQUIRE_FALSE(mh(asio::const_buffer(empty.data(), empty.size()), 
+                      io_interface_mock(iohp), endp));
+        REQUIRE(cnt == 1);
+      }
+    }
+    AND_WHEN ("a msg hdlr is created with reply false and send is called") {
+      test_counter cnt(0);
+      msg_hdlr<io_handler_mock> mh(false, cnt);
+      THEN ("the shutdown message is handled correctly and count is correct") {
+        REQUIRE(mh(asio::const_buffer(msg.data(), msg.size()), io_interface_mock(iohp), endp));
+        REQUIRE_FALSE(mh(asio::const_buffer(empty.data(), empty.size()), 
+                      io_interface_mock(iohp), endp));
+        REQUIRE(cnt == 1);
+      }
+    }
+  } // end given
+}
+
+SCENARIO ( "Message handling shared test utility, msg hdlr function object async stress test",
+           "[msg_handling] [msg_hdlr] [stress]" ) {
+  using namespace chops::test;
+
+  constexpr int sz1 = 2000;
+  constexpr int sz2 = 3000;
+  constexpr int sz3 = 8000;
+
+  GIVEN ("Three types of messages and three large sizes") {
+    WHEN ("a msg hdlr is created and stress tested in separate async calls") {
+      auto cnt1 = std::async(std::launch::async, msg_hdlr_stress_test_variable_len_msg, "Async fun var len msg", 'A', sz1);
+      auto cnt2 = std::async(std::launch::async, msg_hdlr_stress_test_cr_lf_text_msg, "Ha, hilarity cr lf text msg", 'L', sz2);
+      auto cnt3 = std::async(std::launch::async, msg_hdlr_stress_test_lf_text_msg, "Nom, nom lf text msg", 'M', sz3);
+
+      THEN ("the three return values match") {
+        REQUIRE(cnt1.get() == sz1);
+        REQUIRE(cnt2.get() == sz2);
+        REQUIRE(cnt3.get() == sz3);
+      }
+    }
+  } // end given
+}
+
