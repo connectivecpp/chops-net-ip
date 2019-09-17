174c45b2dec24f57b3bed3937066901c533d2ec8
n-deutsch <nate.deutsch@gmail.com>
2019-09-16 19:52:27 -0700

    added non-random exponential backoff to the tcp_connector_timeout class

--- include/net_ip/tcp_connector_timeout.hpp
+++ include/net_ip/tcp_connector_timeout.hpp
@@ -2,7 +2,7 @@
  *
  *  @ingroup net_ip_module
  *
- *  @brief @c default_connector_timeout class using exponential backoff.
+ *  @brief @c default_connector_timeout class
  *
  *  @author Nathan Deutsch
  *
@@ -22,60 +22,39 @@
  #include <string_view>
  #include <string>
  #include <utility>
- #include <cmath> /* pow */
- #include <stdlib.h> /* srand, rand */
- #include <time.h> /* time */
+ #include <math.h>       /* pow */
 
 namespace chops {
 namespace net {
 
   /*
-   *  @brief Class that uses exponentiql backoff to manage timeouts
+   *  @brief Class that handles TCP reconnection attempts
    */
-class tcp_connector_timeout{
+class tcp_connector_timeout {
 public:
-  tcp_connector_timeout((std::chrono::milliseconds initial_timeout, int multiplier = 1, bool reconn=false);
+  tcp_connector_timeout((std::chrono::milliseconds initial_timeout, int max_timeout = (60 * 1000), 
+  int multiplier = 2, bool reconn=false, bool backoff=false);
+
   std::optional<std::chrono::milliseconds> operator()(std::size_t num_connects) {
-    int ms = 0;
-    //implementation here
-    if(!reconn && num_connects > 0){
+    //no reconnection attempt, exit gracefully
+    if(!reconn) {
       return {};
     }
 
+    if(!backoff) { 
+      return std::optional<std::chrono::milliseconds> {initial_timeout};
+    }
+
+    //exponential backoff
+    int ms = pow(multiplier, num_connects);
+    
+    if(ms > max_timeout) {
+      ms = max_timeout;
+    }
+    
     return std::optional<std::chrono::milliseconds> {ms};
-  }
+  } //end operator()
 
-  /*
-   * @brief returns number of milliseconds of delay before retry
-   *
-   * I chose random number generaton for this implementation because
-   * deterministic backoff can result in a large number of clients hitting
-   * in lockstep.
-   */
-  int exponential_backoff(int attempts) {
-    int max_delay = 60 * 1000;
-    int backoff = 0;
-    int interval = 5;
-
-    // no need to wait if this is a first attempt
-    if ( attempts <= 1) {
-      return 0;
-    }
-    /*
-     * after n failed attempts, backoff = x * interval, where
-     * x is a number between 0 and 2^n-1
-    */
-    int limit = pow(2, attempts) - 1;
-
-    srand(time(NULL));
-    multiplier = rand() % limit;
-    backoff = backoff * multiplier;
-
-    if( backoff > max_delay ) {
-      return max_delay;
-    }
-    return backoff;
-  } // end exponential_backoff()
 }; // end default_connector_timeout
 
 } // end net namespace
