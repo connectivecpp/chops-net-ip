e309ffb089e8a90ed338c8de637017f1e57342c9
Thurman Gillespy <thurman.gillespy@comcast.net>
2019-10-28 18:38:08 -0700

    include send_to_all

--- example/chat_server_demo.cpp
+++ example/chat_server_demo.cpp
@@ -34,6 +34,7 @@ chat_server_demo.cpp -lpthread -o chat_server
 #include "net_ip/net_ip.hpp"
 #include "net_ip/net_entity.hpp"
 #include "net_ip_component/worker.hpp"
+#include "net_ip_component/send_to_all.hpp"
 #include "marshall/extract_append.hpp"
 #include "net_ip/io_type_decls.hpp"
 
