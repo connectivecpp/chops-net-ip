cc63c0dd01b83e794c8b4113388302de53300720
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2020-02-19 14:09:11 -0800

    Refactored CMake files, added Doxygen gen CMake.

---
+++ cmake/header_dirs_var.cmake
@@ -0,0 +1,15 @@
+# Copyright 2020 by Cliff Green
+#
+# https://github.com/connectivecpp/utility-rack
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+set ( header_dirs
+    "${include_source_dir}"
+    "${utility_rack_include_source_dir}"
+    "${third_party_include_source_dir}"
+    )
+
+# end of file
+
