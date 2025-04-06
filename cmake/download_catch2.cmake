d50cc89517503858adb01d2d4b61da79e702bc17
cliffg-softwarelibre <softwarelibre@codewrangler.net>
2025-04-06 16:15:38 -0600

    Adding, will remove soon

---
+++ cmake/download_catch2.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2024-2025 by Cliff Green
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+CPMAddPackage ( "gh:catchorg/Catch2@3.8.0" )
+
